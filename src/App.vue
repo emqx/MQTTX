18179a49189f06c7ccfb6f1606a94ce1f730dcd3
CrazyWisdom <zh.whong@gmail.com>
2019-06-24 23:35:15 +0800

    style(tabbar): add tabbar style

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 
 <style lang="scss">
+@import "./assets/font/iconfont.css";
 @import "./assets/scss/base.scss";
 @import "./assets/scss/theme/dark.scss";
 @import "./assets/scss/theme/light.scss";
