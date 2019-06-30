fc7dd47debea3ebcaf568745beb94fbce3d5fbce
CrazyWisdom <zh.whong@gmail.com>
2019-06-30 23:05:37 +0800

    feat(connections): add connections leftbar css style

--- src/views/connections/index.vue
+++ src/views/connections/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <leftbar/>
+    <leftbar>
+      <left-topbar/>
+      <left-list/>
+    </leftbar>
+
     <div class="connections-view right-content">
       <div class="titlebar">Recent Connections</div>
     </div>
@@ -11,10 +15,14 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Leftbar from '@/components/Leftbar.vue'
+import LeftTopbar from './LeftTopbar.vue'
+import LeftList from './LeftList.vue'
 
 @Component({
   components: {
     Leftbar,
+    LeftTopbar,
+    LeftList,
   },
 })
 export default class Connections extends Vue {}
