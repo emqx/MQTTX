7c0f00c10e42f18ea15a3c1666db13868248d5b1
ysfscream <894402575@qq.com>
2019-07-10 15:33:45 +0800

    style(home): refactor style structure

---
+++ src/views/connections/ConnectionsContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="connections-content right-content">
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ConnectionsContent extends Vue {}
+</script>
+
+
+<style lang="scss">
+.connections-content {
+  padding-top: 60px;
+}
+</style>
