1c70b077633020a395f51a748a6bb58eabd1eb9f
CrazyWisdom <zh.whong@gmail.com>
2019-07-15 23:56:25 +0800

    style(BrokersList): add brokers list leftbar style

---
+++ src/views/brokers/BrokersList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="brokers-list">
+    <div class="broker-item">
+      <div>
+        <div class="broker-name">Broker 1</div>
+        <div class="broker-url">127.0.0.1:1883</div>
+      </div>
+    </div>
+    <div class="broker-item active">
+      <div>
+        <div class="broker-name">Broker 2</div>
+        <div class="broker-url">127.0.0.1:8883</div>
+      </div>
+      <div class="ssl"></div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BrokersList extends Vue {}
+</script>
+
+
+<style lang="scss" scope>
+@import "~@/assets/scss/variable.scss";
+
+.brokers-list {
+  .broker-item {
+    display: flex;
+    align-items: center;
+    position: relative;
+    height: 80px;
+    padding: 0 32px;
+    cursor: pointer;
+    &.active {
+      background-color: var(--color-bg-connection_item);
+      border-left: 4px solid var(--color-main-green);
+    }
+    .broker-name {
+      font-size: $font-size--body;
+      font-weight: 500;
+      color: var(--color-text-title);
+    }
+    .broker-url {
+      font-size: $font-size--tips;
+      color: var(--color-text-default);
+    }
+    .ssl {
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 0;
+      height: 0;
+      border-top: 36px solid var(--color-main-green);
+      border-left: 36px solid transparent;
+    }
+  }
+}
+</style>
--- src/views/brokers/index.vue
+++ src/views/brokers/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <Leftbar/>
+    <Leftbar>
+      <SearchTopbar/>
+      <BrokersList/>
+    </Leftbar>
+
     <div class="brokers-view right-content">
       <div class="titlebar">Brokers</div>
     </div>
@@ -11,10 +15,14 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Leftbar from '@/components/Leftbar.vue'
+import SearchTopbar from '@/components/SearchTopbar.vue'
+import BrokersList from './BrokersList.vue'
 
 @Component({
   components: {
     Leftbar,
+    SearchTopbar,
+    BrokersList,
   },
 })
 export default class Brokers extends Vue {}
