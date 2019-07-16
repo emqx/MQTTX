1250317fdc008098809c0b7e396b0a857039eb63
ysfscream <894402575@qq.com>
2019-07-16 23:47:02 +0800

    feat(connections): add payload input

--- src/views/connections/ConnectionsContent.vue
+++ src/views/connections/ConnectionsContent.vue
@@ -6,11 +6,22 @@
         type="primary">
         {{ $t('connections.connectBroker') }}
       </el-button> -->
-      <div class="connect-message">
-        <el-input placeholder="Write a message" @focus="canSend = true"></el-input>
+      <div :class="['connections-input', isEdit ? 'message' : 'message-disabled']">
+        <el-input
+          :placeholder="isEdit ? 'Topic' : 'Write a message'"
+          v-model="msgRecord.topic"
+          @focus="isEdit = true">
+        </el-input>
+        <el-input
+          v-if="isEdit"
+          type="textarea"
+          rows="4"
+          placeholder="Payload"
+          v-model="msgRecord.payload">
+        </el-input>
         <a
           href="javascript:;"
-          :class="['send-btn', canSend ? '' : 'disabled']"
+          class="send-btn"
           @click="sendMsg">
           <i class="iconfont icon-send"></i>
         </a>
@@ -23,14 +34,24 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
+interface Message {
+  topic: string,
+  payload: string,
+}
+
 @Component
 export default class ConnectionsContent extends Vue {
-  private canSend: boolean = false
+  private isEdit: boolean = false
+  private msgRecord: Message = {
+    topic: '',
+    payload: '',
+  }
 
   private sendMsg(): void {
-    if (!this.canSend) {
+    if (!this.isEdit) {
       return
     }
+    this.isEdit = false
   }
 }
 </script>
@@ -55,24 +76,46 @@ export default class ConnectionsContent extends Vue {
       border-radius: 0px;
       font-size: $font-size--title;
     }
-    .connect-message {
-      @include flex-space-between;
-      width: 100%;
-      height: 60px;
-      padding-right: 16px;
+    .connections-input {
       background: var(--color-bg-normal);
       border-top: 2px solid var(--color-border-default);
+      padding: 0px 16px;
+      transition: .3s height;
       .el-input__inner {
         border: 0px;
         border-radius: 0px;
+        padding: 0px;
+      }
+      textarea {
+        resize: none;
+      }
+      .el-textarea__inner {
+        border-color: var(--color-border-default);
+        border-left: 0px;
+        border-right: 0px;
+        border-bottom: 0px;
+        border-radius: 0px;
+        padding: 8px 0px;
       }
       .send-btn {
-        &.disabled {
-          color: var(--color-text-tips);
-          cursor: not-allowed;
-        }
+        position: fixed;
+        right: 16px;
+        bottom: 10px;
         .icon-send {
           font-size: $font-size--send;
+        }
+      }
+      &.message {
+        height: 200px;
+      }
+      &.message-disabled {
+        @include flex-space-between;
+        width: 100%;
+        height: 60px;
+        padding-right: 16px;
+        .send-btn {
+          color: var(--color-text-tips);
+          cursor: not-allowed;
         }
       }
     }
