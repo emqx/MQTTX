af9026c4cb1aa3e64224228c9a209e76f8014cba
ysfscream <894402575@qq.com>
2019-07-18 23:27:22 +0800

    feat(connections): add payload and topic message item

---
+++ src/views/connections/MsgLeftItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="msg-left-item">
+    <p class="left-topic">{{ topic }}</p>
+    <div class="left-payload payload">
+      <pre>{{ payload }}</pre>
+    </div>
+    <p class="left-time time">{{ time }}</p>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class MsgLeftItem extends Vue {
+  @Prop({ required: true }) public topic!: string
+  @Prop({ required: true }) public payload!: string
+  @Prop({ required: true }) public time!: string
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/mixins.scss";
+
+.msg-left-item {
+  text-align: left;
+  @include msg-item;
+  .left-payload {
+    background: var(--color-main-grey);
+    border-radius: 0px 12px 12px 12px;
+  }
+}
+</style>
---
+++ src/views/connections/MsgRightItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="msg-right-item">
+    <p class="right-topic">{{ topic }}</p>
+    <div class="right-payload payload">
+      <pre>{{ payload }}</pre>
+    </div>
+    <p class="right-time time">{{ time }}</p>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class MsgrightItem extends Vue {
+  @Prop({ required: true }) public topic!: string
+  @Prop({ required: true }) public payload!: string
+  @Prop({ required: true }) public time!: string
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/mixins.scss";
+
+.msg-right-item {
+  text-align: right;
+  @include msg-item;
+  .right-payload {
+    color: #fff;
+    background: var(--color-main-green);
+    border-radius: 12px 0 12px 12px;
+    text-align: left;
+  }
+}
+</style>
