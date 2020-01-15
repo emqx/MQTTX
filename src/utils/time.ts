df2ca70a44120dc98802288073b28566f0c2ec60
ysfscream <894402575@qq.com>
2020-01-15 15:46:30 +0800

    refactor(connections): change the unit of connect time to seconds(#153)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,12 +2,18 @@ import moment from 'moment'
 
 interface TimeModel {
   getNowDate: (format?: string) => string
+  convertSecondsToMs: (seconds: number) => number
 }
 
 export const getNowDate = (format: string = 'YYYY-MM-DD HH:mm:ss'): string => moment().format(format)
 
+export const convertSecondsToMs = (seconds: number): number => {
+  return seconds * 1000
+}
+
 const time: TimeModel = {
   getNowDate,
+  convertSecondsToMs,
 }
 
 export default time
