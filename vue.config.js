402a5656a0204139e696649f1f522943f7700d90
ysfscream <894402575@qq.com>
2020-02-17 17:29:13 +0800

    chore(config): mark mqtt package as an external

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      // Prevent bundling of certain imported packages and instead retrieve these external dependencies at runtime.
+      // In order to connect to websocket.
+      externals: ['mqtt'],
       builderOptions: {
         win: {
           icon: './public/app.ico'
