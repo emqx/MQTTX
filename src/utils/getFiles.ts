08ab43add3d8e15b3f7d7a737f67689f26dff5d6
ysfscream <894402575@qq.com>
2020-04-27 23:56:51 +0800

    perf(mqttUtils): imporve the code

--- src/utils/getFiles.ts
+++ src/utils/getFiles.ts
@@ -1,13 +1,12 @@
 import fs from 'fs'
-import path from 'path'
 import { SSLPath, SSLContent } from '@/views/connections/types'
 
 export const getSSLFile = (sslPath: SSLPath): SSLContent | undefined => {
   const { ca, cert, key } = sslPath
   const res: SSLContent = {
-    ca: ca !== '' ? [fs.readFileSync(path.join(ca), 'utf-8')] : undefined,
-    cert: cert !== '' ? fs.readFileSync(path.join(cert), 'utf-8') : undefined,
-    key: key !== '' ? fs.readFileSync(path.join(key), 'utf-8') : undefined,
+    ca: ca !== '' ? [fs.readFileSync(ca)] : undefined,
+    cert: cert !== '' ? fs.readFileSync(cert) : undefined,
+    key: key !== '' ? fs.readFileSync(key) : undefined,
   }
   return res
 }
