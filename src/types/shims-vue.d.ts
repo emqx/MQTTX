874778d6ce8fe2807eb3318278c0e08d6ffc3e49
ysfscream <894402575@qq.com>
2020-12-10 14:18:23 +0800

    refactor(types): replace require to import

--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -2,6 +2,7 @@ declare module 'lodash-id'
 declare module 'uuid'
 declare module 'vue-click-outside'
 declare module 'chart.js'
+declare module 'json2csv'
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
