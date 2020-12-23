947db3f4898cf60b9b5dfde696a577bf24920a0e
oceanlvr <657531018@qq.com>
2020-12-23 10:41:39 +0800

    feat(logs): add log file and console appender

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow, ipcMain, shell, Menu } from 'electron'
 import { createProtocol, installVueDevtools } from 'vue-cli-plugin-electron-builder/lib'
 import { updateConnectionMessage } from '@/api/connection'
+import { getlogger } from './utils/logger'
 import db from './database/index'
 import updateChecker from './main/updateChecker'
 import getMenuTemplate from './main/getMenuTemplate'
@@ -78,6 +79,8 @@ function createWindow() {
     backgroundColor: theme === 'dark' ? '#232323' : '#ffffff',
     icon: `${__static}/app.ico`,
   })
+  const logger = getlogger('main thread', 'info')
+  logger.info('App Init')
 
   // Menu Manger
   const templateMenu = getMenuTemplate(win)
---
+++ src/utils/logger.ts
@@ -0,0 +1,56 @@
+import { configure, getLogger, Logger } from 'log4js'
+import { app, remote } from 'electron'
+import fs from 'fs-extra'
+
+/**
+ * Get a logger above the specified level with scope.
+ * @param scope - the scope of module, you should describe it semantically.
+ * @param level - the level of log getter, logger will only display logs larger than this level.
+ */
+export const getlogger = (scope: string, level: string): Logger => {
+  const isRenderer: boolean = process.type === 'renderer'
+  // Render process use remote app
+  const APP: Electron.App = isRenderer ? remote.app : app
+
+  const STORE_PATH: string = APP.getPath('userData')
+  const LOG_PATH: string = `${STORE_PATH}/logs`
+
+  // In production mode, during the first open application
+  // APP.getPath('userData') gets the path nested.
+  // if it doesn't exist, create it.
+  if (!isRenderer && !fs.pathExistsSync(STORE_PATH)) {
+    fs.mkdirpSync(STORE_PATH)
+  }
+
+  // create dir APP_STORE_PATH/logs if it doesn't exist.
+  if (!fs.pathExistsSync(LOG_PATH)) {
+    fs.mkdirSync(LOG_PATH)
+  }
+
+  // all < trace < debug < info < warn < error < fatal < mark < off
+  configure({
+    appenders: {
+      fileOutput: {
+        type: 'file',
+        filename: `${LOG_PATH}/log`,
+        pattern: 'yyyy-MM-dd-hh.log',
+        alwaysIncludePattern: true,
+        maxLogSize: 102400,
+      },
+      consoleOutput: {
+        type: 'console',
+      },
+    },
+    categories: {
+      default: {
+        appenders: ['fileOutput', 'consoleOutput'],
+        // only output greater than debug level. such as info/warn
+        // set to all if you want to print all level logger
+        level: 'debug',
+      },
+    },
+  })
+  const newLogger = getLogger(scope)
+  newLogger.level = level
+  return newLogger
+}
