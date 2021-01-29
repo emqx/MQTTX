767cab3ded7c828b473452fd75aa7d97391df6c9
oceanlvr <657531018@qq.com>
2021-01-29 15:28:44 +0800

    fix(MsgPublish): fix multiple editors that were not destroyed, causing failure to bind payload
    Signed-off-by: oceanlvr <657531018@qq.com>

--- src/components/MsgPublish.vue
+++ src/components/MsgPublish.vue
@@ -109,10 +109,13 @@ export default class MsgPublish extends Vue {
   }
   @Watch('$route.params.id', { immediate: true, deep: true })
   private handleIdChanged(to: string, from: string) {
+    const editorRef = this.$refs.payloadEditor as Editor
     if (from === '0' && to !== '0') {
       // Init the editor when rout jump from creation page
-      const editorRef = this.$refs.payloadEditor as Editor
       editorRef.initEditor()
+    } else if (from !== '0' && to === '0') {
+      // destroy the editor when rout jump to creation page
+      editorRef.destroyEditor()
     }
   }
 
