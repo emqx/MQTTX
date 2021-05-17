f7f1a9d0add92c14e4cbd037284ac8df541a2446
YuShifan <894402575bt@gmail.com>
2021-05-17 14:37:59 +0800

    feat(about): add emqx cloud link

--- src/components/EmptyPage.vue
+++ src/components/EmptyPage.vue
@@ -4,9 +4,10 @@
       <div>
         <img :src="imageSrc" alt="new connection" />
       </div>
-      <el-button type="outline" icon="el-icon-plus" @click="clickMethod(false)">
+      <el-button class="primary-btn" icon="el-icon-plus" @click="clickMethod(false)">
         {{ btnTitle }}
       </el-button>
+      <p v-html="$t('common.cloud')"></p>
     </div>
   </div>
 </template>
@@ -21,7 +22,7 @@ export default class EmptyPage extends Vue {
   @Prop() public clickMethod!: <T>() => T | void
 
   get imageSrc(): string {
-    return require(`../assets/images/${this.name}.png`)
+    return require(`../assets/images/${this.name}`)
   }
 }
 </script>
@@ -29,13 +30,19 @@ export default class EmptyPage extends Vue {
 <style lang="scss" scoped>
 .empty-page {
   .empty-page__block {
+    .primary-btn {
+      background: linear-gradient(134deg, #37dc85 0%, #35ca8d 100%);
+      color: #fff;
+      border: 1px solid #37dc85;
+    }
     img {
-      width: 220px;
-      height: 135px;
       margin-bottom: 20px;
     }
     text-align: center;
     padding-top: 30%;
+    p {
+      margin-top: 32px;
+    }
   }
 }
 </style>
