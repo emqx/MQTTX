76dcf0d26e79c5a56893378c7a4630ba501fafe2
YuShifan <894402575bt@gmail.com>
2021-05-21 18:16:56 +0800

    chore(deps): bump vue-cli-plugin-electron-builder from 1.4.6 to 2.0.0

--- vue.config.js
+++ vue.config.js
@@ -53,6 +53,7 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
+      nodeIntegration: true,
       // Prevent bundling of certain imported packages and instead retrieve these external dependencies at runtime.
       // In order to connect to websocket.
       externals: ['mqtt', 'vm2', 'log4js'],
