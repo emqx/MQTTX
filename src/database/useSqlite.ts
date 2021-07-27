f90628dbe5608e08a8d93b32573e0f705ff90f6e
oceanlvr <657531018@qq.com>
2021-07-27 11:46:13 +0800

    fix(db): fix dataconfig model import issue

--- src/database/useSqlite.ts
+++ src/database/useSqlite.ts
@@ -1,9 +1,9 @@
-import { createConnection, EntityTarget, getConnectionOptions } from 'typeorm'
+import { createConnection, EntityTarget } from 'typeorm'
+import ORMConfig from './database.config'
 
 export default function useSqlite() {
   const insert = async <T>(data: T, model: EntityTarget<T>): Promise<T> => {
-    const config = await getConnectionOptions('MQTTX')
-    const db = await createConnection(config)
+    const db = await createConnection(ORMConfig)
     const repository = db.getRepository(model)
     return repository.save(data)
   }
