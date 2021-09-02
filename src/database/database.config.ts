e264c845f32db5ff2fd147bd9527fdeb64638f35
oceanlvr <657531018@qq.com>
2021-09-02 16:57:29 +0800

    migration(connection): create historyConnections entity

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -9,11 +9,13 @@ import SettingEntity from './models/SettingEntity'
 import CollectionEntity from './models/CollectionEntity'
 import HistoryMessageHeaderEntity from './models/HistoryMessageHeaderEntity'
 import HistoryMessagePayloadEntity from './models/HistoryMessagePayloadEntity'
+import HistoryConnectionEntity from './models/HistoryConnectionEntity'
 import WillEntity from './models/WillEntity'
 import { ConnectionOptions } from 'typeorm'
 import { initTable1629476510574 } from './migration/1629476510574-initTable'
 import { messages1630403733964 } from './migration/1630403733964-messages'
 import { collection1630497638704 } from './migration/1630497638704-collection'
+import { historyConnections1630572256038 } from './migration/1630572256038-historyConnections'
 
 const STORE_PATH = getAppDataPath('MQTTX')
 try {
@@ -28,12 +30,11 @@ const ORMConfig = {
   type: 'sqlite',
   driver: 'sqlite',
   synchronize: false,
-  // logging: process.env.NODE_ENV !== 'production',
-  logging: false,
+  logging: process.env.NODE_ENV !== 'production',
   database: join(STORE_PATH, 'MQTTX.db'),
   cache: true,
   timezone: 'Z',
-  migrations: [initTable1629476510574, messages1630403733964, collection1630497638704],
+  migrations: [initTable1629476510574, messages1630403733964, collection1630497638704, historyConnections1630572256038],
   migrationsTableName: 'temp_migration_table',
   entities: [
     ConnectionEntity,
@@ -45,6 +46,7 @@ const ORMConfig = {
     HistoryMessageHeaderEntity,
     HistoryMessagePayloadEntity,
     WillEntity,
+    HistoryConnectionEntity,
   ],
   cli: {
     migrationsDir: 'src/database/migration',
