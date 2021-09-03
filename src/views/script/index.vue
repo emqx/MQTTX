6c8c7ba623c5f6a00078b0394d7e092082a72df3
oceanlvr <657531018@qq.com>
2021-09-03 17:04:20 +0800

    fix(connectionDetail): fix connection detail scroll issue

--- src/views/script/index.vue
+++ src/views/script/index.vue
@@ -101,7 +101,6 @@ import Editor from '@/components/Editor.vue'
 import MyDialog from '@/components/MyDialog.vue'
 import sandbox from '@/utils/sandbox'
 import useServers from '@/database/useServices'
-import settings from '@/lang/settings'
 
 @Component({
   components: {
