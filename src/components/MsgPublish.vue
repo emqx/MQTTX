44aea79e45fec0d366a4f8d2d62a6a086187935d
lileiemq <lei.li@emqx.io>
2021-09-24 17:21:13 +0800

    fix(msgPublish): Data format conversion error

--- src/components/MsgPublish.vue
+++ src/components/MsgPublish.vue
@@ -243,6 +243,9 @@ export default class MsgPublish extends Vue {
     const { historyMessageHeaderService, historyMessagePayloadService } = useServices()
     const headersHistory = (await historyMessageHeaderService.getAll()) ?? []
     const payloadsHistory = (await historyMessagePayloadService.getAll()) ?? []
+    if (payloadsHistory[payloadsHistory.length - 1]) {
+      this.payloadType = payloadsHistory[payloadsHistory.length - 1].payloadType
+    }
     this.headersHistory = headersHistory
     this.payloadsHistory = payloadsHistory
     if (isNewPayload) {
