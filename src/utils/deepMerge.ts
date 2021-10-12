7fa7c656069bd1a45c96b17a2712ef5f5a391ec4
oceanlvr <657531018@qq.com>
2021-10-12 14:51:24 +0800

    fix(connectionForm): enhance custom deepMerge method

--- src/utils/deepMerge.ts
+++ src/utils/deepMerge.ts
@@ -3,6 +3,7 @@ interface MergeObjModel {
 }
 
 const deepMerge = (target: MergeObjModel, source: MergeObjModel) => {
+  if (!target) return
   for (const key of Object.keys(source)) {
     if (source[key] instanceof Object) {
       Object.assign(source[key], deepMerge(target[key], source[key]))
