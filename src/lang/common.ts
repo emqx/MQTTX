aa3a63247a4cfe818a4255ceb3dbfaa1f5e0714e
YuShifan <894402575bt@gmail.com>
2021-12-09 10:33:38 +0800

    fix(common): modify emqx cloud description

--- src/lang/common.ts
+++ src/lang/common.ts
@@ -218,7 +218,7 @@ export default {
   },
   cloud: {
     zh: '需要一个云原生的全托管 MQTT 服务？一键部署 <a href="https://www.emqx.com/zh/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloud</a>',
-    en: 'Need a Cloud-Native fully-managed MQTT broker? Try <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloud</a> now!',
+    en: 'Need a Fully Managed, Cloud-Native MQTT Messaging Service? Try <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloud</a> now!',
     tr: 'Cloud-Native, tam olarak yönetilen bir MQTT aracısına mı ihtiyacınız var? Deneyin <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloud</a> şimdi!',
     ja: 'Need a Cloud-Native fully-managed MQTT broker? Try <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloud</a> now!',
     hu: 'Szüksége van egy Cloud-Native teljesen menedzselt MQTT Brókerre? Próbálja ki az <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=2021" target="_blank" rel="noopener noreferrer">EMQ X Cloudot</a> most! ',
