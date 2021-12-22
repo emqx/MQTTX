093171981f3d33904eb11b3306ac2f9be87a8708
YuShifan <894402575bt@gmail.com>
2021-12-22 14:49:28 +0800

    feat(connection): display response topic correlation data and topic alias

--- src/components/MsgRightItem.vue
+++ src/components/MsgRightItem.vue
@@ -5,20 +5,31 @@
         <span class="topic">Topic: {{ topic }}</span>
         <span class="qos">QoS: {{ qos }}</span>
       </p>
-      <p v-if="properties.subscriptionIdentifier" class="properties right">
-        <span>{{ $t('connections.subscriptionIdentifier') }}: {{ properties.subscriptionIdentifier }}</span>
-      </p>
-      <p v-if="properties.contentType" class="properties right">
-        <span>{{ $t('connections.contentType') }}: {{ properties.contentType }}</span>
-      </p>
-      <p v-if="properties.userProperties" class="user-properties right">
-        <KeyValueEditor
-          class="msg-item-props"
-          :title="$t('connections.userProperties')"
-          :disabled="true"
-          :value="properties.userProperties"
-        />
-      </p>
+      <div class="meta">
+        <p v-if="properties.subscriptionIdentifier" class="properties right">
+          <span>{{ $t('connections.subscriptionIdentifier') }}: {{ properties.subscriptionIdentifier }}</span>
+        </p>
+        <p v-if="properties.contentType" class="properties right">
+          <span>{{ $t('connections.contentType') }}: {{ properties.contentType }}</span>
+        </p>
+        <p v-if="properties.topicAlias" class="properties right">
+          <span>{{ $t('connections.topicAlias') }}: {{ properties.topicAlias }}</span>
+        </p>
+        <p v-if="properties.responseTopic" class="properties right">
+          <span>{{ $t('connections.responseTopic') }}: {{ properties.responseTopic }}</span>
+        </p>
+        <p v-if="properties.correlationData" class="properties right">
+          <span>{{ $t('connections.correlationData') }}: {{ properties.correlationData }}</span>
+        </p>
+        <p v-if="properties.userProperties" class="user-properties properties right">
+          <KeyValueEditor
+            class="msg-item-props"
+            :title="$t('connections.userProperties')"
+            :disabled="true"
+            :value="properties.userProperties"
+          />
+        </p>
+      </div>
       <pre>{{ payload }}</pre>
     </div>
     <p class="right-time time">{{ createAt }}</p>
