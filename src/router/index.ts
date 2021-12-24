c655742454d7d50b53282906124ebe12f24dc1d0
YuShifan <894402575bt@gmail.com>
2021-12-24 17:45:39 +0800

    chore(package): bump mqtt to 4.3.0 to fix types error

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ const router: Router = new Router({
   mode: 'hash',
   base: process.env.BASE_URL,
   scrollBehavior(to, from, savedPosition) {
-    if (to.meta.keepAlive && savedPosition) {
+    if (to.meta?.keepAlive && savedPosition) {
       return savedPosition
     }
     return { x: 0, y: 0 }
