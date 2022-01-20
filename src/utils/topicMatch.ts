634c701460c4a1e87d935fcf7959a9f3283610ad
YuShifan <894402575bt@gmail.com>
2022-01-20 23:06:09 +0800

    feat(topicMatch): support shared subscription marker topic colors

--- src/utils/topicMatch.ts
+++ src/utils/topicMatch.ts
@@ -1,8 +1,19 @@
+/**
+ * Topic matching algorithm
+ * @return Return matched result, true or false
+ * @param filter - type: topic string, subscription list topics
+ * @param topic - type: topic string, real send-receive topics
+ */
 export const matchTopicMethod = (filter: string, topic: string): boolean => {
-  // Topic matching algorithm
-  const filterArray: string[] = filter.split('/')
+  let _filter = filter
+  let _topic = topic
+  if (filter.includes('$share')) {
+    // shared subscription format: $share/{ShareName}/{filter}
+    _filter = filter.split('/').slice(2).join('/')
+  }
+  const filterArray: string[] = _filter.split('/')
   const length: number = filterArray.length
-  const topicArray: string[] = topic.split('/')
+  const topicArray: string[] = _topic.split('/')
   for (let i = 0; i < length; i += 1) {
     const left: string = filterArray[i]
     const right: string = topicArray[i]
