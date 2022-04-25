461cf3063eb51e47f59a6359e989a572b11ae3cc
Asuka <2586632362@qq.com>
2022-04-25 10:17:52 +0800

    docs(about): apply suggestions from code review
    Co-authored-by: YuShifan <894402575bt@gmail.com>

--- src/lang/about.ts
+++ src/lang/about.ts
@@ -36,7 +36,7 @@ export default {
   },
   mqttxDesc: {
     zh: 'MQTT X 是一款由 EMQ 开源的 MQTT 5.0 跨平台桌面客户端，旨在帮助开发者更快的开发、调试 MQTT 服务和应用。',
-    en: 'MQTT X is an open source MQTT 5.0 cross-platform desktop client from EMQ, designed to help developers develop and debug MQTT services and applications faster.',
+    en: 'MQTT X is a cross-platform MQTT 5.0 client tool open-sourced by EMQ, designed to help develop and debug MQTT services and applications faster.,
     tr: 'MQTT X, geliştiricilerin MQTT hizmetlerini ve uygulamalarını daha hızlı geliştirmelerine ve hata ayıklamalarına yardımcı olmak için tasarlanmış, EMQ tarafından sağlanan açık kaynaklı bir MQTT 5.0 platformlar arası masaüstü istemcisidir.',
     ja: 'MQTT Xは、EMQによるオープンソースのMQTT 5.0クロスプラットフォーム・デスクスクライアントで、開発者がMQTTサービスおよびアプリケーションをより速く開発およびデバッグできるよう設計されています。',
     hu: 'Az MQTT X egy nyílt forráskódú MQTT 5.0 cross-platform asztali kliens az EMQ-tól, amelynek célja, hogy segítse a fejlesztőket az MQTT szolgáltatások és alkalmazások gyorsabb fejlesztésében és hibakeresésében.',
@@ -50,7 +50,7 @@ export default {
   },
   cloudSummary: {
     zh: 'EMQX Cloud 是由 EMQ 提供的全托管 MQTT 云服务平台，可连接海量物联网设备并实时处理数据，且支持按量付费。',
-    en: 'EMQX Cloud is a fully managed MQTT cloud service platform provided by EMQ, which can connect massive IoT devices and process data in real time, and supports pay-as-you-go.',
+    en: 'EMQX Cloud is a fully managed MQTT cloud service platform provided by EMQ that connects massive amounts of IoT devices and processes data in real-time with pay-per-use support.',
     tr: 'EMQX Cloud, büyük IoT cihazlarını bağlayabilen ve verileri gerçek zamanlı olarak işleyebilen ve kullandıkça öde özelliğini destekleyen, EMQ tarafından sağlanan, tam olarak yönetilen bir MQTT bulut hizmeti platformudur.',
     ja: 'EMQX Cloudは、EMQが提供する、膨大な数のIoTデバイスを接続し、リアルタイムにデータ処理を行うフルマネージドMQTTクラウドサービスプラットフォームで、従量課金制に対応しているのが特徴です。',
     hu: 'Az EMQX Cloud az EMQ által nyújtott, teljes körűen menedzselt MQTT felhőszolgáltatási platform, amely nagyszámú IoT-eszközt kapcsol össze és valós időben dolgozza fel az adatokat, használatonként fizetős támogatással.',
