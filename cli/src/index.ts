9300c2ac151ee0bd949863d78e5b850d563943df
Red-Asuka <liyd0623@gmail.com>
2022-07-21 14:08:59 +0800

    feat(cli): add connect command

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander'
 import { getClientId } from './utils/generator'
 import { parseNumber, parseProtocol, parseMQTTVersion, parseUserProperties } from './utils/parse'
+import conn from './lib/conn'
 import pub from './lib/pub'
 import sub from './lib/sub'
 import { version } from '../package.json'
@@ -20,10 +21,37 @@ export class Commander {
       .version(version, '-v, --version')
 
     this.program
+      .command('conn')
+      .description('Create a connection and connect to MQTT Broker.')
+      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
+      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
+      .option('-p, --port <PORT>', 'the broker port', parseNumber)
+      .option('-i, --client-id <ID>', 'the client id', getClientId())
+      .option('--clean', 'discard any pending message for the given id', true)
+      .option('-k, --keepalive <SEC>', 'send a ping every SEC seconds', parseNumber, 30)
+      .option('-u, --username <USER>', 'the username')
+      .option('-P, --password <PASS>', 'the password')
+      .option('-l, --protocol <PROTO>', 'the protocol to use, mqtt, mqtts, ws or wss', parseProtocol)
+      .option('--key <PATH>', 'path to the key file')
+      .option('--cert <PATH>', 'path to the cert file')
+      .option('--ca <PATH>', 'path to the ca certificate')
+      .option('--insecure', 'do not verify the server certificate')
+      .option(
+        '-up, --user-properties <USERPROPERTIES...>',
+        'properties of will by MQTT 5.0 (e.g. -up "name: mqttx cli")',
+        parseUserProperties,
+      )
+      .option('--will-topic <TOPIC>', 'the will topic')
+      .option('--will-message <BODY>', 'the will message')
+      .option('--will-qos <0/1/2>', 'the will qos', parseNumber)
+      .option('--will-retain', 'send a will retained message', false)
+      .action(conn)
+
+    this.program
       .command('pub')
       .description('Publish a message to a topic.')
       .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
-      .option('-h, --hostname <HOST>', 'the broker host')
+      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
       .option('-p, --port <PORT>', 'the broker port', parseNumber)
       .option('-i, --client-id <ID>', 'the client id', getClientId())
       .option('-q, --qos <0/1/2>', 'the QoS of the message', parseNumber, 0)
@@ -53,7 +81,7 @@ export class Commander {
     this.program
       .command('sub')
       .description('Subscribes to a topic.')
-      .option('-V, --mqtt-version <5.0/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
+      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
       .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
       .option('-p, --port <PORT>', 'the broker port', parseNumber)
       .option('-i, --client-id <ID>', 'the client id', getClientId())
