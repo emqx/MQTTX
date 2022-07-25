285590f1032ff7db34810aab7a7c859b7cb639fe
Red-Asuka <liyd0623@gmail.com>
2022-07-25 16:11:47 +0800

    chore(vetur): add vetur monorepos config

---
+++ vetur.config.js
@@ -0,0 +1,14 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    'vetur.useWorkspaceDependencies': true,
+    'vetur.experimental.templateInterpolationService': true,
+  },
+  // **optional** default: `[{ root: './' }]`
+  // support monorepos
+  projects: ['./', './web', './cli'],
+}
