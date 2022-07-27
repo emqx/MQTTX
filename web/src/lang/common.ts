6dbf0c40a6fa7ab66ce066762b0964597d7804b6
Red-Asuka <liyd0623@gmail.com>
2022-07-27 14:18:08 +0800

    feat(web): add version & releases link

--- web/src/lang/common.ts
+++ web/src/lang/common.ts
@@ -104,6 +104,11 @@ export default {
     en: 'Advanced Config',
     ja: '詳細設定',
   },
+  version: {
+    zh: '版本：',
+    en: 'Version: ',
+    ja: 'システムバージョン：',
+  },
   cloud: {
     zh: '需要一个云原生的全托管 MQTT 服务？一键部署 <a href="https://www.emqx.com/zh/cloud/?utm_source=emqx.io&utm_medium=referral&utm_campaign=mqttx-web-to-cloud" target="_blank" rel="noopener noreferrer">EMQX Cloud</a>！14 天免费试用！',
     en: 'Need a Fully Managed, Cloud-Native MQTT Messaging Service? Try <a href="https://www.emqx.com/en/cloud/?utm_source=emqx.io&utm_medium=referral&utm_campaign=mqttx-web-to-cloud" target="_blank" rel="noopener noreferrer">EMQX Cloud</a> now! 14-day trial, no credit card required.',
