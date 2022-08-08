fcb3bb1c21ea564affbbb378dd1fd3d276ed12fd
Red-Asuka <liyd0623@gmail.com>
2022-08-08 13:56:06 +0800

    chore(deps): add vue virtual scroller

--- web/src/types/shims-vue.d.ts
+++ web/src/types/shims-vue.d.ts
@@ -2,6 +2,7 @@ declare module 'lodash-id'
 declare module 'jump.js'
 declare module 'uuid'
 declare module 'vue-click-outside'
+declare module 'vue-virtual-scroller'
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
