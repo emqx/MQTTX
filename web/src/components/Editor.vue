76193c49883683a8e7208a1003338dc9ccaf5f03
Red-Asuka <liyd0623@gmail.com>
2022-08-10 14:52:20 +0800

    feat(web): sync editor component

--- web/src/components/Editor.vue
+++ web/src/components/Editor.vue
@@ -8,14 +8,26 @@ import { Getter } from 'vuex-class'
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
 import EditorDark from '@/assets/scss/theme/editor-dark.json'
 import EditorNight from '@/assets/scss/theme/editor-night.json'
+import LogEditor from '@/assets/scss/theme/custom/log-editor.json'
+import LogEditorDark from '@/assets/scss/theme/custom/log-editor-dark.json'
+import LogEditorNight from '@/assets/scss/theme/custom/log-editor-night.json'
+import LogEditorRules from '@/assets/scss/theme/custom/log-editor-rules.json'
 
 @Component
 export default class Editor extends Vue {
   @Prop({ required: true }) public id!: string
   @Prop({ required: true }) public lang!: string
-  @Prop({ default: 14 }) public fontSize!: number
-  @Prop({ default: 'hidden' }) public scrollbarStatus: 'auto' | 'visible' | 'hidden' | undefined
-
+  @Prop({ default: 12 }) public fontSize!: number
+  @Prop({ default: 'off' }) public lineNumbers!: 'off' | 'on'
+  @Prop({ default: 1 }) public lineNumbersMinChars!: number
+  @Prop({ default: 'none' }) public renderHighlight!: 'none' | 'line'
+  @Prop({ default: 'auto' }) public scrollbarStatus: 'auto' | 'visible' | 'hidden' | undefined
+  @Prop({ default: false }) public useShadows!: boolean
+  @Prop({ default: false }) public disabled!: boolean
+  @Prop({ default: undefined }) public editorTheme!: Theme
+  @Prop({ default: false }) public isCustomerLang!: boolean
+  @Prop({ default: 'off' }) public wordWrap!: 'off' | 'on'
+  @Prop({ default: undefined }) public lineHeight!: number
   @Model('change', { type: String }) private readonly value!: string
 
   @Getter('currentTheme') private theme!: Theme
@@ -31,6 +43,13 @@ export default class Editor extends Vue {
     }
   }
 
+  @Watch('theme')
+  private handleThemeChanged(val: Theme) {
+    this.editor?.updateOptions({
+      theme: this.editorTheme || this.getTheme(),
+    })
+  }
+
   @Watch('lang')
   private handleLangChanged(val: string) {
     if (this.editor) {
@@ -39,18 +58,67 @@ export default class Editor extends Vue {
     }
   }
 
-  private initEditor(): void | boolean {
+  @Watch('disabled')
+  private handleDisabledChanged(val: string) {
+    if (this.editor) {
+      this.editor.dispose()
+      this.initEditor()
+    }
+  }
+
+  // init and register customer editor style
+  private initCustomerLanguages() {
+    this.registerLog()
+  }
+
+  // register log style editor
+  private registerLog() {
+    monaco.languages.register({ id: 'logLanguage' })
+    // set tokens for the logLanguage
+    monaco.languages.setMonarchTokensProvider('logLanguage', {
+      tokenizer: {
+        root: [
+          [/\[TRACE\]/i, 'custom-trace'],
+          [/\[DEBUG\]/i, 'custom-debug'],
+          [/\[INFO\]/i, 'custom-info'],
+          [/\[WARN\]/i, 'custom-warn'],
+          [/\[ERROR\]/i, 'custom-error'],
+          [/\[FATAL\]/i, 'custom-fatal'],
+          [/\[[0-9]{4}-[0-9]{2}-[0-9]{2} [a-zA-Z.0-9:]+\]/i, 'custom-date'],
+        ],
+      },
+    })
+  }
+
+  public scrollToBottom() {
+    if (!this.editor) {
+      return
+    }
+    const thisEditorModel = this.editor.getModel()
+    if (!thisEditorModel) return
+    const maxLine = thisEditorModel.getLineCount() || 0
+    this.editor.revealLine(maxLine)
+  }
+
+  public initEditor(): void | boolean {
+    // if customer editorTheme is not empty, then init the editor theme
+    if (this.isCustomerLang) {
+      this.initCustomerLanguages()
+    }
     const defaultOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
       value: this.value,
       language: this.lang,
-      readOnly: false,
+      readOnly: this.disabled,
+      wordWrap: this.wordWrap,
+      lineHeight: this.lineHeight,
       fontSize: this.fontSize,
       scrollBeyondLastLine: false,
-      lineNumbers: 'off',
-      renderLineHighlight: 'none',
+      lineNumbers: this.lineNumbers,
+      lineNumbersMinChars: this.lineNumbersMinChars,
+      renderLineHighlight: this.renderHighlight,
       matchBrackets: 'near',
       folding: false,
-      theme: this.getTheme(),
+      theme: this.editorTheme || this.getTheme(),
       lightbulb: {
         enabled: false,
       },
@@ -59,10 +127,13 @@ export default class Editor extends Vue {
       },
       scrollbar: {
         horizontal: this.scrollbarStatus,
+        horizontalScrollbarSize: 8,
         vertical: this.scrollbarStatus,
-        useShadows: false,
+        verticalScrollbarSize: 8,
+        useShadows: this.useShadows,
         alwaysConsumeMouseWheel: false,
       },
+      smoothScrolling: true,
     }
     // Create
     const id = document.getElementById(`monaco-${this.id}`)
@@ -85,6 +156,9 @@ export default class Editor extends Vue {
     this.editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter, () => {
       this.$emit('enter-event', this.value)
     })
+    this.editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
+      this.$emit('qucik-save', this.value)
+    })
     // Update editor options
     const model = this.editor.getModel()
     if (model) {
@@ -96,8 +170,10 @@ export default class Editor extends Vue {
     this.editor.onDidBlurEditorText(() => {
       this.$emit('blur')
     })
+    // Add contextmenu item
+    this.addContextmenuItem()
   }
-  private editorLayout() {
+  public editorLayout() {
     if (this.editor) {
       this.editor.layout()
     }
@@ -107,14 +183,37 @@ export default class Editor extends Vue {
     const night = EditorNight as monaco.editor.IStandaloneThemeData
     monaco.editor.defineTheme('editor-dark', dark)
     monaco.editor.defineTheme('editor-night', night)
+    if (this.isCustomerLang) {
+      const log = { ...LogEditor, ...LogEditorRules } as monaco.editor.IStandaloneThemeData
+      const logDark = { ...LogEditorDark, ...LogEditorRules } as monaco.editor.IStandaloneThemeData
+      const logNight = { ...LogEditorNight, ...LogEditorRules } as monaco.editor.IStandaloneThemeData
+      // customer language theme
+      monaco.editor.defineTheme('editor-log', log)
+      monaco.editor.defineTheme('editor-log-dark', logDark)
+      monaco.editor.defineTheme('editor-log-night', logNight)
+    }
+  }
+  private addContextmenuItem() {
+    if (this.lang === 'json' && this.editor) {
+      this.editor.addAction({
+        id: 'beautifyFormat',
+        label: 'Beautify format',
+        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_B],
+        contextMenuGroupId: '9_cutcopypaste',
+        run: () => {
+          this.$emit('format')
+        },
+      })
+    }
   }
   private getTheme(): string {
-    if (this.theme === 'dark') {
-      return 'editor-dark'
-    } else if (this.theme === 'night') {
-      return 'editor-night'
-    } else {
-      return 'vs'
+    switch (this.theme) {
+      case 'dark':
+        return 'editor-dark'
+      case 'night':
+        return 'editor-night'
+      default:
+        return 'vs'
     }
   }
 
@@ -127,7 +226,8 @@ export default class Editor extends Vue {
       this.editorLayout()
     }
   }
-  private beforeDestroy() {
+
+  public destroyEditor() {
     if (this.editor) {
       const model = this.editor.getModel()
       if (model) {
@@ -136,6 +236,10 @@ export default class Editor extends Vue {
       this.editor.dispose()
       this.editor = null
     }
+  }
+
+  private beforeDestroy() {
+    this.destroyEditor()
   }
 }
 </script>
