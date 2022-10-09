8bd4d8a3c96b501855dd6a739cc73dc880d6b1cd
Red-Asuka <liyd0623@gmail.com>
2022-10-09 16:40:08 +0800

    feat(cli): support setting the connect user properties in pub or sub command

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -86,19 +86,16 @@ export class Commander {
     this.program
       .command('pub')
       .description('Publish a message to a topic.')
-      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
-      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
-      .option('-p, --port <PORT>', 'the broker port', parseNumber)
-      .option('-i, --client-id <ID>', 'the client id', getClientId())
-      .option('-q, --qos <0/1/2>', 'the QoS of the message', parseNumber, 0)
       .requiredOption('-t, --topic <TOPIC>', 'the message topic', parsePubTopic)
-      .option('-m, --message <MSG>', 'the message body', 'Hello From MQTT X CLI')
+      .option('-m, --message <BODY>', 'the message body', 'Hello From MQTT X CLI')
+      .option('-q, --qos <0/1/2>', 'the QoS of the message', parseNumber, 0)
       .option('-r, --retain', 'send a retained message')
       .option('-d, --dup', 'mark as duplicate flag')
       .option('-s, --stdin', 'read the message body from stdin')
       .option('-M, --multiline', 'read lines from stdin as multiple messages')
+      // properties options of MQTT 5.0
       .option('-pf, --payload-format-indicator', 'the payload format indicator of the publish message')
-      .option('-me, --message-expiry-interval <NUMBER>', 'the lifetime of the publish message in seconds', parseNumber)
+      .option('-e, --message-expiry-interval <NUMBER>', 'the lifetime of the publish message in seconds', parseNumber)
       .option(
         '-ta, --topic-alias <NUMBER>',
         'value that is used to identify the topic instead of using the topic name',
@@ -109,8 +106,20 @@ export class Commander {
         '-cd, --correlation-data <DATA>',
         'used by the sender of the request message to identify which request the response message is for when it is received',
       )
+      .option(
+        '-up, --user-properties <USERPROPERTIES...>',
+        'the user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
+        parseUserProperties,
+      )
       .option('-si, --subscription-identifier <NUMBER>', 'the identifier of the subscription', parseNumber)
       .option('-ct, --content-type <TYPE>', 'a description of the content of the publish message')
+      // connect options
+      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
+      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
+      .option('-p, --port <PORT>', 'the broker port', parseNumber)
+      .option('-i, --client-id <ID>', 'the client id', getClientId())
+      .option('--no-clean', 'set the clean session flag to false (default: true)')
+      .option('-k, --keepalive <SEC>', 'send a ping every SEC seconds', parseNumber, 30)
       .option('-u, --username <USER>', 'the username')
       .option('-P, --password <PASS>', 'the password')
       .option('-l, --protocol <PROTO>', 'the protocol to use, mqtt or mqtts (default: mqtt)', parseProtocol)
@@ -118,6 +127,7 @@ export class Commander {
       .option('--cert <PATH>', 'path to the cert file')
       .option('--ca <PATH>', 'path to the ca certificate')
       .option('--insecure', 'do not verify the server certificate')
+      // connect properties options of MQTT 5.0
       .option('-se, --session-expiry-interval <SECONDS>', 'the session expiry interval in seconds', parseNumber)
       .option('--rcv-max, --receive-maximum <NUMBER>', 'the receive maximum value', parseNumber)
       .option('--maximum-packet-size <NUMBER>', 'the maximum packet size the client is willing to accept', parseNumber)
@@ -125,30 +135,35 @@ export class Commander {
       .option('--req-response-info', 'the client requests response information from the server')
       .option('--no-req-problem-info', 'the client requests problem information from the server')
       .option(
-        '-up, --user-properties <USERPROPERTIES...>',
-        'the user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
+        '-Cup, --conn-user-properties <USERPROPERTIES...>',
+        'the connect user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
         parseUserProperties,
       )
-      .option('--will-topic <TOPIC>', 'the will topic')
-      .option('--will-message <BODY>', 'the will message')
-      .option('--will-qos <0/1/2>', 'the will qos', parseNumber, 0)
-      .option('--will-retain', 'send a will retained message')
+      // will message options
+      .option('-Wt, --will-topic <TOPIC>', 'the will topic')
+      .option('-Wm, --will-message <BODY>', 'the will message')
+      .option('-Wq, --will-qos <0/1/2>', 'the will qos', parseNumber)
+      .option('-Wr, --will-retain', 'send a will retained message')
+      // will message properties options of MQTT 5.0
+      .option('-Wd, --will-delay-interval <SECONDS>', 'the will delay interval in seconds', parseNumber)
+      .option('-Wpf, --will-payload-format-indicator', 'will message is UTF-8 encoded character data or not')
+      .option('-We, --will-message-expiry-interval <SECONDS>', 'lifetime of the will message in seconds', parseNumber)
+      .option('-Wct, --will-content-type <CONTENTTYPE>', 'description of the will message’s content')
+      .option('-Wrt, --will-response-topic <TOPIC>', 'topic name for a response message')
+      .option('-Wcd, --will-correlation-data <DATA>', 'correlation data for the response message')
+      .option(
+        '-Wup, --will-user-properties <USERPROPERTIES...>',
+        'the user properties of will message',
+        parseUserProperties,
+      )
       .action(pub)
 
     this.program
       .command('sub')
       .description('Subscribes to a topic.')
-      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
-      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
-      .option('-p, --port <PORT>', 'the broker port', parseNumber)
-      .option('-i, --client-id <ID>', 'the client id', getClientId())
-      .option('-q, --qos <0/1/2...>', 'the QoS of the message', parseQoS)
-      .option('--no-clean', 'set the clean session flag to false (default: true)')
       .requiredOption('-t, --topic <TOPIC...>', 'the message topic')
-      .option('-k, --keepalive <SEC>', 'send a ping every SEC seconds', parseNumber, 30)
-      .option('-u, --username <USER>', 'the username')
-      .option('-P, --password <PASS>', 'the password')
-      .option('-l, --protocol <PROTO>', 'the protocol to use, mqtt or mqtts (default: mqtt)', parseProtocol)
+      .option('-q, --qos <0/1/2...>', 'the QoS of the message', parseQoS)
+      // properties options of MQTT 5.0
       .option('-nl, --no_local [FLAG...]', 'the no local MQTT 5.0 flag', parseVariadicOfBooleanType)
       .option(
         '-rap, --retain-as-published [FLAG...]',
@@ -157,10 +172,27 @@ export class Commander {
       )
       .option('-rh, --retain-handling <0/1/2...>', 'the retain handling MQTT 5.0', parseQoS)
       .option('-si, --subscription-identifier <NUMBER...>', 'the identifier of the subscription', parseNumber)
+      .option(
+        '-up, --user-properties <USERPROPERTIES...>',
+        'the user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
+        parseUserProperties,
+      )
+      .option('-v, --verbose', 'print the topic before the message')
+      // connect options
+      .option('-V, --mqtt-version <5/3.1.1/3.1>', 'the MQTT version', parseMQTTVersion, 5)
+      .option('-h, --hostname <HOST>', 'the broker host', 'localhost')
+      .option('-p, --port <PORT>', 'the broker port', parseNumber)
+      .option('-i, --client-id <ID>', 'the client id', getClientId())
+      .option('--no-clean', 'set the clean session flag to false (default: true)')
+
+      .option('-u, --username <USER>', 'the username')
+      .option('-P, --password <PASS>', 'the password')
+      .option('-l, --protocol <PROTO>', 'the protocol to use, mqtt or mqtts (default: mqtt)', parseProtocol)
       .option('--key <PATH>', 'path to the key file')
       .option('--cert <PATH>', 'path to the cert file')
       .option('--ca <PATH>', 'path to the ca certificate')
       .option('--insecure', 'do not verify the server certificate')
+      // connect properties options of MQTT 5.0
       .option('-se, --session-expiry-interval <SECONDS>', 'the session expiry interval in seconds', parseNumber)
       .option('--rcv-max, --receive-maximum <NUMBER>', 'the receive maximum value', parseNumber)
       .option('--maximum-packet-size <NUMBER>', 'the maximum packet size the client is willing to accept', parseNumber)
@@ -168,15 +200,27 @@ export class Commander {
       .option('--req-response-info', 'the client requests response information from the server')
       .option('--no-req-problem-info', 'the client requests problem information from the server')
       .option(
-        '-up, --user-properties <USERPROPERTIES...>',
-        'the user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
+        '-Cup, --conn-user-properties <USERPROPERTIES...>',
+        'the connect user properties of MQTT 5.0 (e.g. -up "name: mqttx cli")',
         parseUserProperties,
       )
-      .option('--will-topic <TOPIC>', 'the will topic')
-      .option('--will-message <BODY>', 'the will message')
-      .option('--will-qos <0/1/2>', 'the will qos', parseNumber)
-      .option('--will-retain', 'send a will retained message')
-      .option('-v, --verbose', 'print the topic before the message')
+      // will message options
+      .option('-Wt, --will-topic <TOPIC>', 'the will topic')
+      .option('-Wm, --will-message <BODY>', 'the will message')
+      .option('-Wq, --will-qos <0/1/2>', 'the will qos', parseNumber)
+      .option('-Wr, --will-retain', 'send a will retained message')
+      // will message properties options of MQTT 5.0
+      .option('-Wd, --will-delay-interval <SECONDS>', 'the will delay interval in seconds', parseNumber)
+      .option('-Wpf, --will-payload-format-indicator', 'will message is UTF-8 encoded character data or not')
+      .option('-We, --will-message-expiry-interval <SECONDS>', 'lifetime of the will message in seconds', parseNumber)
+      .option('-Wct, --will-content-type <CONTENTTYPE>', 'description of the will message’s content')
+      .option('-Wrt, --will-response-topic <TOPIC>', 'topic name for a response message')
+      .option('-Wcd, --will-correlation-data <DATA>', 'correlation data for the response message')
+      .option(
+        '-Wup, --will-user-properties <USERPROPERTIES...>',
+        'the user properties of will message',
+        parseUserProperties,
+      )
       .action(sub)
   }
 }
