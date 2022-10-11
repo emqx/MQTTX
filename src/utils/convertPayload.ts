96e1cee21c649782b817f487c4e66de07bed4cfa
YuShifan <894402575bt@gmail.com>
2022-10-11 10:07:14 +0800

    feat(connection): support hex format

--- src/utils/convertPayload.ts
+++ src/utils/convertPayload.ts
@@ -18,10 +18,12 @@ const convertBase64 = (value: string, codeType: 'encode' | 'decode'): string => 
 const convertHex = (value: string, codeType: 'encode' | 'decode'): string => {
   const convertMap: CodeType = {
     encode(str: string): string {
-      return Buffer.from(str, 'utf-8').toString('hex')
+      return Buffer.from(str, 'utf-8')
+        .toString('hex')
+        .replace(/(.{4})/g, '$1 ')
     },
     decode(str: string): string {
-      return Buffer.from(str, 'hex').toString('utf-8')
+      return Buffer.from(str.replaceAll(' ', ''), 'hex').toString('utf-8')
     },
   }
   return convertMap[codeType](value)
@@ -39,25 +41,25 @@ const convertJSON = (value: string): Promise<string> =>
   })
 
 const convertPayload = async (payload: string, currentType: PayloadType, fromType: PayloadType): Promise<string> => {
-  let _payload = payload
+  let $payload = payload
   switch (fromType) {
     case 'Base64':
-      _payload = convertBase64(payload, 'decode')
+      $payload = convertBase64(payload, 'decode')
       break
     case 'Hex':
-      _payload = convertHex(payload, 'decode')
+      $payload = convertHex(payload, 'decode')
       break
   }
   if (currentType === 'Base64') {
-    _payload = convertBase64(_payload, 'encode')
+    $payload = convertBase64($payload, 'encode')
   }
   if (currentType === 'JSON') {
-    _payload = await convertJSON(_payload)
+    $payload = await convertJSON($payload)
   }
   if (currentType === 'Hex') {
-    _payload = convertHex(_payload, 'encode')
+    $payload = convertHex($payload, 'encode')
   }
-  return _payload
+  return $payload
 }
 
 export default convertPayload
