cf95a771aa8fcb3114a1d3793fffab98f11ef3cd
Red-Asuka <liyd0623@gmail.com>
2022-11-01 14:43:52 +0800

    chore(help): add utm params to links on help page

--- src/utils/gaCustomLinks.ts
+++ src/utils/gaCustomLinks.ts
@@ -9,6 +9,10 @@ export default (lang: Language) => {
   const EMQCNSite = 'https://emqx.com/zh'
   const EMQSite = lang === 'zh' ? EMQCNSite : EMQENSite
 
+  const forumENSite = 'https://github.com/emqx/MQTTX/discussions'
+  const forumCNSite = 'https://askemq.com/c/tools/11'
+  const forumSite = lang === 'zh' ? forumCNSite : forumENSite
+
   const utm = '?utm_source=mqttx&utm_medium=referral&utm_campaign='
 
   const leftBarLogo = `${MQTTXSite}${utm}logo-to-homepage`
@@ -23,11 +27,20 @@ export default (lang: Language) => {
     EMQXCloud: `${EMQSite}/cloud${utm}mqttx-to-cloud`,
   }
 
+  const help = {
+    docs: `${MQTTXSite}/docs${utm}mqttx-help-to-docs`,
+    forum: `${forumSite}${utm}mqttx-help-to-forum`,
+    learnMQTT: `${EMQSite}/mqtt${utm}mqttx-help-to-learn-mqtt`,
+    blogUtm: `${utm}mqttx-help-to-blog`,
+  }
+
   return {
     MQTTXSite,
     EMQSite,
+    forumSite,
     leftBarLogo,
     empty,
     about,
+    help,
   }
 }
