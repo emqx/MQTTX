6d762d25070cc4bd94a1756f6164b90966abd9b6
Red-Asuka <liyd0623@gmail.com>
2022-12-23 10:34:18 +0800

    docs(help): update MQTT beginner's guide

--- web/src/lang/help.ts
+++ web/src/lang/help.ts
@@ -32,9 +32,9 @@ export default {
     ja: '豊富で分かりやすい MQTT ガイドで、MQTT プロトコルをすぐに始めることができます。',
   },
   guideArticle1: {
-    zh: '物流网首选协议，关于 MQTT 你需要了解这些',
-    en: 'What is the MQTT protocol',
-    ja: 'What is the MQTT protocol',
+    zh: '物联网首选协议，关于 MQTT 你需要了解这些',
+    en: 'What is The MQTT and Why is it the Best Protocol for IoT?',
+    ja: 'What is The MQTT and Why is it the Best Protocol for IoT?',
   },
   guideArticle2: {
     zh: 'MQTT 协议快速体验',
@@ -47,29 +47,39 @@ export default {
     ja: 'Introduction to MQTT publish-subscribe model',
   },
   guideArticle4: {
+    zh: '创建 MQTT 连接时如何设置参数？',
+    en: 'How to Set Parameters When Establishing an MQTT Connection?',
+    ja: 'How to Set Parameters When Establishing an MQTT Connection?',
+  },
+  guideArticle5: {
     zh: '通过案例理解 MQTT 主题与通配符',
     en: 'Understanding MQTT Topics & Wildcards by Case',
     ja: 'Understanding MQTT Topics & Wildcards by Case',
   },
-  guideArticle5: {
-    zh: 'MQTT QoS（服务质量）介绍',
-    en: 'Introduction to MQTT QoS (Quality of Service)',
-    ja: 'Introduction to MQTT QoS (Quality of Service)',
-  },
   guideArticle6: {
-    zh: 'MQTT 会话',
+    zh: 'MQTT 持久会话与 Clean Session 详解',
     en: 'MQTT Session',
     ja: 'MQTT Session',
   },
   guideArticle7: {
-    zh: 'MQTT 保留消息是什么？如何使用？',
-    en: 'MQTT Retain Message',
-    ja: 'MQTT Retain Message',
+    zh: 'MQTT QoS（服务质量）介绍',
+    en: 'Introduction to MQTT QoS (Quality of Service)',
+    ja: 'Introduction to MQTT QoS (Quality of Service)',
   },
   guideArticle8: {
+    zh: 'MQTT 保留消息是什么？如何使用？',
+    en: "The Beginner's Guide to MQTT Retained Messages",
+    ja: "The Beginner's Guide to MQTT Retained Messages",
+  },
+  guideArticle9: {
     zh: 'MQTT 遗嘱消息（Will Message）的使用',
     en: 'Use of MQTT Will Message',
     ja: 'Use of MQTT Will Message',
+  },
+  guideArticle10: {
+    zh: 'MQTT 协议 Keep Alive 详解',
+    en: 'What is the MQTT Keep Alive parameter for?',
+    ja: 'What is the MQTT Keep Alive parameter for?',
   },
   practiceTitle: {
     zh: 'MQTT 编程',
