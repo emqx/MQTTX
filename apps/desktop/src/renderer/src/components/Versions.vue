9f17d4e442bdfdf794629b36b53e35573771a344
YuShifan <894402575bt@gmail.com>
2023-10-20 17:19:21 +0800

    refactor(scss): reset all styles

--- apps/desktop/src/renderer/src/components/Versions.vue
+++ apps/desktop/src/renderer/src/components/Versions.vue
@@ -5,8 +5,8 @@ const versions = reactive({ ...window.electron.process.versions })
 
 <template>
   <ul class="versions">
-    <li class="electron-version text-8xl text-green-primary">Electron v{{ versions.electron }}</li>
-    <li class="chrome-version text-5xl">Chromium v{{ versions.chrome }}</li>
+    <li class="electron-version text-2xl text-green-primary">Electron v{{ versions.electron }}</li>
+    <li class="chrome-version text-2xl text-black-primary">Chromium v{{ versions.chrome }}</li>
     <li class="node-version">Node v{{ versions.node }}</li>
     <li class="v8-version">V8 v{{ versions.v8 }}</li>
   </ul>
