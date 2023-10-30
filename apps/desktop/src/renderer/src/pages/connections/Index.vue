d2cce1a9920abbc55ea92ada1a5e277f9885466f
YuShifan <894402575bt@gmail.com>
2023-10-30 10:50:03 +0800

    feat(*): add shared icons

--- apps/desktop/src/renderer/src/pages/connections/Index.vue
+++ apps/desktop/src/renderer/src/pages/connections/Index.vue
@@ -3,6 +3,7 @@ import useMockData from '@/composables/useMockData'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useConnectionsStore } from '@mqttx/ui'
+import { Plus } from '@element-plus/icons-vue'
 
 const connStore = useConnectionsStore()
 const { connections } = useMockData()
@@ -11,9 +12,9 @@ const activeId = computed(() => route.params.id)
 </script>
 
 <template>
-  <div class="p-3 text-xl">Connections Count: {{ connStore.count }}</div>
+  <div class="p-3 text-xl">Connections Count: {{ connStore.count + 1 }}</div>
   <div class="p-3 text-xl">
-    <el-button class="w-[100px]" @click="connStore.increment()">+</el-button>
+    <el-button :icon="Plus" class="w-[100px]" @click="connStore.increment()">Add</el-button>
   </div>
   <div class="connections-container">
     <div class="connections-list">
--- packages/ui/src/components/connections/ListView.vue
+++ packages/ui/src/components/connections/ListView.vue
@@ -14,7 +14,13 @@ const connStore = useConnectionsStore()
   <div class="connection-list-view">
     {{ connStore.count }}
     <ul v-if="data.length">
+      <svg class="icon w-[24px] h-[24px]" aria-hidden="true">
+        <use xlink:href="#icon-unfold"></use>
+      </svg>
       <li v-for="{ id, name } in data" :key="id">
+        <svg class="icon w-[24px] h-[24px]" aria-hidden="true">
+          <use xlink:href="#icon-fold"></use>
+        </svg>
         <router-link :to="`/connections/${id}`" :class="[activeId === id ? 'text-green-primary' : '', 'text-base']">
           {{ name }}
         </router-link>
