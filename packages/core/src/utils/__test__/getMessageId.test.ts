b7ac8abebc4d6c0d9281e2d524ffd79113cc09ec
YuShifan <894402575bt@gmail.com>
2023-11-04 01:12:36 +0800

    feat(packages): add unit tests to core and ui

---
+++ packages/core/src/utils/__test__/getMessageId.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+import { getMessageId } from '../getMessageId'
+
+describe('getMessageId', () => {
+  it('should return a string that starts with "message_" followed by a UUIDv4', () => {
+    const id = getMessageId()
+
+    expect(typeof id).toBe('string')
+
+    expect(id.startsWith('message_')).toBe(true)
+
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    const uuid = id.split('message_')[1]
+    expect(uuid).toMatch(uuidRegex)
+  })
+})
