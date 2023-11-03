2e9f7e283cc84bddc49608be559a21d3c2e84330
YuShifan <894402575bt@gmail.com>
2023-11-03 11:22:12 +0800

    feat(ui): add hover styles to split view

--- packages/ui/src/components/common/SplitView.vue
+++ packages/ui/src/components/common/SplitView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, defineProps, computed } from 'vue'
+import { ref, onMounted, defineProps, computed, type PropType } from 'vue'
 
 const props = defineProps({
   fixedPanelSize: {
@@ -17,6 +17,15 @@ const props = defineProps({
   vertical: {
     type: Boolean,
     default: false,
+  },
+  hidePanelName: {
+    type: String as PropType<'panel-1' | 'panel-2'>,
+    default: '',
+  },
+  // TainwindCSS backgroud color class
+  handleColor: {
+    type: String,
+    default: '',
   },
 })
 
@@ -45,13 +54,22 @@ const containerStyles = computed(() => {
   return base
 })
 const resizeHandleStyles = computed(() => {
-  const base = 'bg-border-default z-10'
+  const base = 'relative'
   if (props.vertical) {
     return `${base} w-full h-[1px] cursor-row-resize`
   } else {
     return `${base} w-[1px] h-full cursor-col-resize`
   }
 })
+const resizeHandleHoverStyles = computed(() => [
+  'absolute',
+  'transition-all',
+  props.handleColor,
+  props.vertical ? 'w-full top-1/2 -translate-y-1/2' : 'h-full left-1/2 -translate-x-1/2',
+  !isResizing.value
+    ? [props.vertical ? 'h-[1px] hover:h-[4px] hover:bg-main-green' : 'w-[1px] hover:w-[4px] hover:bg-main-green']
+    : [props.vertical ? 'h-[4px] bg-main-green' : 'w-[4px] bg-main-green'],
+])
 
 const getPanelStyles = () => {
   if (props.vertical) {
@@ -78,16 +96,14 @@ const startResizing = (event: MouseEvent) => {
 const handleMouseMove = (event: MouseEvent) => {
   if (!isResizing.value || !resizePanel.value?.parentElement) return
 
-  const containerSize = props.vertical
-    ? resizePanel.value.parentElement.offsetHeight
-    : resizePanel.value.parentElement.offsetWidth
+  const { offsetHeight, offsetWidth } = resizePanel.value.parentElement
+  const containerSize = props.vertical ? offsetHeight : offsetWidth
   const minSize = parseSize(props.minSize, containerSize)
   const maxSize = parseSize(props.maxSize, containerSize)
-
   const movement = props.vertical ? event.clientY : event.clientX
   const delta = movement - startValue
-  let newSize = startSize + delta
 
+  let newSize = startSize + delta
   if (props.vertical) {
     const maxPanelSize = containerSize - minSize
     const minPanelSize = containerSize - maxSize
@@ -96,7 +112,9 @@ const handleMouseMove = (event: MouseEvent) => {
     newSize = Math.max(minSize, Math.min(newSize, maxSize))
   }
 
-  panelSize.value = newSize
+  requestAnimationFrame(() => {
+    panelSize.value = newSize
+  })
 }
 
 const stopResizing = () => {
@@ -120,11 +138,13 @@ onMounted(() => {
 
 <template>
   <div :class="containerStyles">
-    <div ref="resizePanel" class="panel-1" :style="getPanelStyles()">
+    <div v-if="hidePanelName !== 'panel-1'" ref="resizePanel" class="panel-1" :style="getPanelStyles()">
       <slot name="panel-1"></slot>
     </div>
-    <div :class="resizeHandleStyles" @mousedown="startResizing"></div>
-    <div class="panel-2 flex-1">
+    <div :class="resizeHandleStyles" @mousedown="startResizing">
+      <div :class="resizeHandleHoverStyles"></div>
+    </div>
+    <div v-if="hidePanelName !== 'panel-2'" class="panel-2 flex-1">
       <slot name="panel-2"></slot>
     </div>
   </div>
--- packages/ui/src/components/connections/ListView.vue
+++ packages/ui/src/components/connections/ListView.vue
@@ -11,12 +11,18 @@ const isActive = (id: string): boolean => id === props.activeId
 
 <template>
   <div class="connection-list-view p-4">
+    <div class="list-header mb-3">
+      <h1>{{ $t('connections.connections') }}</h1>
+    </div>
     <ul v-if="data.length">
       <li v-for="{ id, name } in data" :key="id" class="flex items-center">
         <svg class="icon w-[24px] h-[24px] mr-2" aria-hidden="true">
           <use :xlink:href="isActive(id) ? '#icon-unfold' : '#icon-fold'"></use>
         </svg>
-        <router-link :to="`/connections/${id}`" :class="[isActive(id) ? 'text-main-green' : '', 'text-base']">
+        <router-link
+          :to="`/connections/${id}`"
+          :class="['truncate overflow-hidden text-base', isActive(id) ? 'text-main-green' : '']"
+        >
           {{ name }}
         </router-link>
       </li>
