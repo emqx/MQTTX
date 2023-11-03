12e0784ec0f258abd7e51520daa96c9d033374d3
YuShifan <894402575bt@gmail.com>
2023-11-03 13:39:30 +0800

    refactor(ui): rename the leftbar to leftmenu

--- apps/desktop/src/renderer/src/App.vue
+++ apps/desktop/src/renderer/src/App.vue
@@ -9,7 +9,7 @@ console.log('MQTTX Desktop App init...')
 <template>
   <el-config-provider :locale="ElementI18nMap[settingsStore.lang]">
     <el-container>
-      <left-bar />
+      <left-menu />
       <main-view />
     </el-container>
   </el-config-provider>
