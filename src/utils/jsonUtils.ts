75dd168cc915c799ecf45ceec0245d2c6101b80c
ni00 <mail@nixiaobai.com>
2023-12-07 19:20:50 +0800

    fix:throw an error for invaild json string

--- src/utils/jsonUtils.ts
+++ src/utils/jsonUtils.ts
@@ -5,10 +5,15 @@ export const jsonParse: typeof JSON.parse = (...args: any[]) => {
   try {
     // When JSON only contains integers, use the native bigint type.
     return JSONBigInt.parse(...args)
-  } catch (_error) {
-    // When JSON contains floating-point numbers, use the bignumber library.
-    // The numbers in the parsed JSON Object will be represented as BigNumber Objects.
-    return JSONBigNumber.parse(...args)
+  } catch {
+    try {
+      // When JSON contains floating-point numbers, use the bignumber library.
+      // The numbers in the parsed JSON Object will be represented as BigNumber Objects.
+      return JSONBigNumber.parse(...args)
+    } catch {
+      // To verify the validity of the JSON string and throw an error if it's invalid.
+      return JSON.parse(args[0], args[1])
+    }
   }
 }
 
