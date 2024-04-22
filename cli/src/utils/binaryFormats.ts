7a97e4b6ee70d193b474763ea652913be0683b31
DM1-1 <ming.deng@emqx.io>
2024-04-22 16:16:18 +0800

    feat(cli): support for binary format for message format

---
+++ cli/src/utils/binaryFormats.ts
@@ -0,0 +1,14 @@
+const supportedBinaryFormatsForMQTT = [
+  '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.ico', '.tif', '.tiff', // Image file formats
+  '.mp4', '.avi', '.mov', '.mkv', '.flv', '.wmv', '.mpeg', '.3gp', // Video file formats
+  '.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.m4p', // Audio file formats
+  '.zip', '.gz', '.rar', '.tar', '.7z', '.bz2', '.xz', '.jar', // Compressed file formats
+  '.bin', '.exe', '.dll', '.so', '.dmg', '.iso', '.img', // Binary file formats
+  '.pdf', '.epub', // Binary document file formats
+]
+
+const isSupportedBinaryFormatForMQTT = (fileExtname: string) => {
+  return supportedBinaryFormatsForMQTT.includes(fileExtname)
+}
+
+export default isSupportedBinaryFormatForMQTT
