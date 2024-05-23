411f59724db9512dd0b9e399eea608c701bcfd25
YuShifan <894402575bt@gmail.com>
2024-05-23 10:43:06 +0800

    feat(cli): improve check update with ora

--- cli/src/utils/checkUpdate.ts
+++ cli/src/utils/checkUpdate.ts
@@ -1,16 +1,20 @@
 import axios from 'axios'
 import chalk from 'chalk'
 import { compareVersions } from 'compare-versions'
-
 import { version } from '../../package.json'
+import ora from 'ora'
+
+const spinner = ora()
 
 const checkUpdate = async () => {
   try {
+    spinner.start('Checking for updates...')
     const tagsUrl = 'https://community-sites.emqx.com/api/v1/all_version?product=MQTTX'
     const tagsRes = await axios.get(tagsUrl)
     if (tagsRes.status === 200) {
       const latestVersion = tagsRes.data.data[0].replace('v', '')
       if (compareVersions(latestVersion, version) > 0) {
+        spinner.stop()
         console.log(
           chalk.yellow(
             `A new version of MQTTX CLI is available: ${chalk.cyan(version)} ${chalk.reset('→')} ${chalk.cyan(
@@ -19,11 +23,14 @@ const checkUpdate = async () => {
           ),
         )
       } else {
-        console.log(chalk.green('There are currently no updates available.'))
+        spinner.succeed('There are currently no updates available.')
       }
     }
   } catch (error) {
-    console.error(error)
+    const err = error as Error
+    spinner.fail(err.message.toString())
+  } finally {
+    spinner.stop()
   }
 }
 
