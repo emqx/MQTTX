f7fca4fe091a5a32a94949b810f87c6e6e93fd35
YuShifan <894402575bt@gmail.com>
2024-09-28 00:39:15 +0800

    feat(desktop): add tree component & atom css support

--- src/lang/viewer.ts
+++ src/lang/viewer.ts
@@ -6,4 +6,18 @@ export default {
     ja: 'ビューア',
     hu: 'Megjelenítő',
   },
+  topicsTree: {
+    zh: '主题树',
+    en: 'Topics Tree',
+    tr: 'Konular Ağacı',
+    ja: 'トピックツリー',
+    hu: 'Témafa',
+  },
+  filterDesc: {
+    zh: '输入关键字进行过滤',
+    en: 'Input keyword to filter',
+    tr: 'Filtrelemek için anahtar kelimeyi girin',
+    ja: 'フィルタリングするためにキーワードを入力してください',
+    hu: 'Adja meg a kulcsszót a szűréshez',
+  },
 }
