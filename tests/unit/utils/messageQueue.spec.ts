be022514c7d322005edbc41c241e265c5a048747
YuShifan <894402575bt@gmail.com>
2024-10-18 10:20:48 +0800

    test(desktop): update message queue buffer test cases

--- tests/unit/utils/messageQueue.spec.ts
+++ tests/unit/utils/messageQueue.spec.ts
@@ -1,123 +1,128 @@
-// import { expect } from 'chai'
-// import { MessageQueue } from '@/utils/messageQueue'
-// import { take, toArray } from 'rxjs/operators'
+import { expect } from 'chai'
+import { MessageQueue } from '@/utils/messageQueue'
+import { take, toArray } from 'rxjs/operators'
 
-// describe('MessageQueue', () => {
-//   let messageQueue: MessageQueue<any>
+describe('MessageQueue', () => {
+  let messageQueue: MessageQueue<any>
 
-//   beforeEach(() => {
-//     messageQueue = new MessageQueue(100)
-//   })
+  beforeEach(() => {
+    messageQueue = new MessageQueue(100)
+  })
 
-//   it('should buffer and emit individual messages', (done) => {
-//     const messages = ['message1', 'message2', 'message3']
+  it('should buffer and emit batches of messages', (done) => {
+    const messages = ['message1', 'message2', 'message3']
 
-//     messageQueue
-//       .getMessageObservable()
-//       .pipe(take(3), toArray())
-//       .subscribe({
-//         next: (results) => {
-//           expect(results).to.deep.equal(messages)
-//         },
-//         complete: () => {
-//           done()
-//         },
-//         error: (err) => {
-//           done(err)
-//         },
-//       })
+    messageQueue
+      .getMessageObservable()
+      .pipe(take(1))
+      .subscribe({
+        next: (results) => {
+          expect(results).to.deep.equal(messages)
+        },
+        complete: () => {
+          done()
+        },
+        error: (err) => {
+          done(err)
+        },
+      })
 
-//     messages.forEach((msg) => messageQueue.queueMessage(msg))
-//   })
+    messages.forEach((msg) => messageQueue.queueMessage(msg))
+  })
 
-//   it('should apply the handler function if provided', (done) => {
-//     const handler = (msg: string) => msg.toUpperCase()
-//     const customMessageQueue = new MessageQueue(100, handler)
-//     const messages = ['hello', 'world']
+  it('should not emit when no messages are queued', (done) => {
+    let emitted = false
+    messageQueue.getMessageObservable().subscribe({
+      next: () => {
+        emitted = true
+      },
+    })
 
-//     customMessageQueue
-//       .getMessageObservable()
-//       .pipe(take(2), toArray())
-//       .subscribe({
-//         next: (results) => {
-//           expect(results).to.deep.equal(['HELLO', 'WORLD'])
-//         },
-//         complete: () => {
-//           done()
-//         },
-//         error: (err) => {
-//           done(err)
-//         },
-//       })
+    setTimeout(() => {
+      expect(emitted).to.be.false
+      done()
+    }, 200)
+  })
 
-//     messages.forEach((msg) => customMessageQueue.queueMessage(msg))
-//   })
+  it('should use the specified buffer time', (done) => {
+    const bufferTime = 300
+    const customMessageQueue = new MessageQueue(bufferTime)
+    const message = 'test message'
 
-//   it('should not emit when no messages are queued', (done) => {
-//     let emitted = false
-//     messageQueue.getMessageObservable().subscribe({
-//       next: () => {
-//         emitted = true
-//       },
-//     })
+    const start = Date.now()
+    customMessageQueue
+      .getMessageObservable()
+      .pipe(take(1))
+      .subscribe({
+        next: () => {
+          const elapsed = Date.now() - start
+          expect(elapsed).to.be.at.least(bufferTime)
+          done()
+        },
+        error: (err) => {
+          done(err)
+        },
+      })
 
-//     setTimeout(() => {
-//       expect(emitted).to.be.false
-//       done()
-//     }, 200)
-//   })
+    customMessageQueue.queueMessage(message)
+  })
 
-//   it('should use the specified buffer time', (done) => {
-//     const bufferTime = 300
-//     const customMessageQueue = new MessageQueue(bufferTime)
-//     const message = 'test message'
+  it('should handle complex objects', (done) => {
+    interface ComplexMessage {
+      id: number
+      data: string
+    }
 
-//     const start = Date.now()
-//     customMessageQueue
-//       .getMessageObservable()
-//       .pipe(take(1))
-//       .subscribe({
-//         next: () => {
-//           const elapsed = Date.now() - start
-//           expect(elapsed).to.be.at.least(bufferTime)
-//           done()
-//         },
-//         error: (err) => {
-//           done(err)
-//         },
-//       })
+    const complexMessageQueue = new MessageQueue<ComplexMessage>()
+    const messages: ComplexMessage[] = [
+      { id: 1, data: 'first' },
+      { id: 2, data: 'second' },
+      { id: 3, data: 'third' },
+    ]
 
-//     customMessageQueue.queueMessage(message)
-//   })
+    complexMessageQueue
+      .getMessageObservable()
+      .pipe(take(1))
+      .subscribe({
+        next: (results) => {
+          expect(results).to.deep.equal(messages)
+        },
+        complete: () => {
+          done()
+        },
+        error: (err) => {
+          done(err)
+        },
+      })
 
-//   it('should handle complex objects', (done) => {
-//     interface ComplexMessage {
-//       id: number
-//       data: string
-//     }
+    messages.forEach((msg) => complexMessageQueue.queueMessage(msg))
+  })
 
-//     const complexMessageQueue = new MessageQueue<ComplexMessage>()
-//     const messages: ComplexMessage[] = [
-//       { id: 1, data: 'first' },
-//       { id: 2, data: 'second' },
-//       { id: 3, data: 'third' },
-//     ]
+  it('should emit multiple batches', (done) => {
+    const batch1 = ['message1', 'message2']
+    const batch2 = ['message3', 'message4']
 
-//     complexMessageQueue
-//       .getMessageObservable()
-//       .pipe(take(3), toArray())
-//       .subscribe({
-//         next: (results) => {
-//           expect(results).to.deep.equal(messages)
-//         },
-//         complete: () => {
-//           done()
-//         },
-//         error: (err) => {
-//           done(err)
-//         },
-//       })
+    let batchCount = 0
+    messageQueue
+      .getMessageObservable()
+      .pipe(take(2), toArray())
+      .subscribe({
+        next: (results) => {
+          expect(results).to.deep.equal([batch1, batch2])
+          batchCount = results.length
+        },
+        complete: () => {
+          expect(batchCount).to.equal(2)
+          done()
+        },
+        error: (err) => {
+          done(err)
+        },
+      })
 
-//     messages.forEach((msg) => complexMessageQueue.queueMessage(msg))
-//   })
-// })
+    batch1.forEach((msg) => messageQueue.queueMessage(msg))
+    setTimeout(() => {
+      batch2.forEach((msg) => messageQueue.queueMessage(msg))
+    }, 150) // Ensure this is greater than the buffer time
+  })
+})
