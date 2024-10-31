bf4ac74869fe267bb593c7fcb05f6f8bf7f4ad91
Red-Asuka <liyd0623@gmail.com>
2024-10-31 10:48:51 +0800

    feat(web): support for automatic component import

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueJsx()],
+  plugins: [
+    VueRouter(),
+    vue(),
+    vueJsx(),
+    Components({
+      dts: true,
+      directoryAsNamespace: true,
+      dirs: ['src/components', '../../packages/ui/src/components'],
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
