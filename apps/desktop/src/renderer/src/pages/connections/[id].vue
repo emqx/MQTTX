c55811cc8b812ddb4a55665f98bdf28f6386c1b2
Red-Asuka <liyd0623@gmail.com>
2024-10-31 11:24:31 +0800

    feat(desktop): support for automatic component import

--- apps/desktop/src/renderer/src/pages/connections/[id].vue
+++ apps/desktop/src/renderer/src/pages/connections/[id].vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+// import { computed } from 'vue'
+// import { useRoute } from 'vue-router'
 
-const route = useRoute()
-const id = computed(() => route.name === '/connections/[id]' && route.params.id)
+// const route = useRoute()
+// const id = computed(() => route.name === '/connections/[id]' && route.params.id)
 </script>
 
 <template>
-  <connection-details-view
+  TODO: Connection Details View
+  <!-- <ConnectionsDetailsView
     :connection="{
       name: 'test',
       id,
     }"
-  />
+  /> -->
 </template>
--- packages/ui/src/components/common/MainView.vue
+++ packages/ui/src/components/common/MainView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
   <el-main>
     <router-view></router-view>
   </el-main>
 </template>
-
-<style lang="scss" scoped></style>
