52becb9702c2341f89c0259beaf6fe75c1b58055
YuShifan <894402575bt@gmail.com>
2024-11-14 23:40:20 +0800

    fix(cli): support more biarny type files

--- cli/src/utils/binaryFormats.ts
+++ cli/src/utils/binaryFormats.ts
@@ -40,6 +40,29 @@ export const supportedBinaryFormatsForMQTT = [
   '.img', // Binary file formats
   '.pdf',
   '.epub', // Binary document file formats
+  '.netp', // Network protocol binary format
+  '.pcap', // Packet capture
+  '.pcapng', // Next generation packet capture
+  '.cap', // Network capture
+  '.dmp', // Memory dump
+  '.trace', // Network trace
+  '.db', // Generic database
+  '.sqlite', // SQLite database
+  '.mdb', // Microsoft Access database
+  '.frm', // MySQL table definition
+  '.myd', // MySQL data
+  '.myi', // MySQL index
+  '.dat', // Generic data file
+  '.raw', // Raw data
+  '.log', // Binary log
+  '.blob', // Binary large object
+  '.hex', // Hex dump
+  '.rom', // ROM image
+  '.fw', // Firmware
+  '.pkt', // Packet data
+  '.mpack', // MessagePack data
+  '.parquet', // Parquet columnar storage
+  '.orc', // Optimized Row Columnar
 ]
 
 const isSupportedBinaryFormatForMQTT = (fileExtname: string) => {
