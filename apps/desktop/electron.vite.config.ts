f10257f5593b407e91d39d611dedf6aed1b1cecb
Red-Asuka <liyd0623@gmail.com>
2024-11-18 14:31:46 +0800

    chore(scss): silencing warnings

--- apps/desktop/electron.vite.config.ts
+++ apps/desktop/electron.vite.config.ts
@@ -51,5 +51,12 @@ export default defineConfig({
         },
       }),
     ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
   },
 })
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -46,4 +46,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 })
