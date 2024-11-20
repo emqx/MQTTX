91da1f79ab871be8f612886fb99b92c1550af3f2
Red-Asuka <liyd0623@gmail.com>
2024-11-20 10:49:07 +0800

    feat(settings): define the setting store

--- packages/types/base.ts
+++ packages/types/base.ts
@@ -8,6 +8,27 @@ export type Lang = 'en' | 'zh' | 'ja' | 'hu' | 'tr'
 
 export type Theme = 'light' | 'dark' | 'night'
 
+export interface Settings {
+  // General
+  currentLang: Lang
+  autoCheck: boolean
+  autoResub: boolean
+  multiTopics: boolean
+  maxReconnectTimes: number
+  // Appearance
+  syncOsTheme: boolean
+  currentTheme: Theme
+  jsonHighlight: boolean
+  // Advanced
+  logLevel: 'debug' | 'info' | 'warn' | 'error'
+  ignoreQoS0Message: boolean
+  // MQTTX Copilot
+  enableCopilot: boolean
+  openAIAPIHost: string
+  openAIAPIKey: string
+  model: string
+}
+
 export default {}
 
 export interface Connection {
