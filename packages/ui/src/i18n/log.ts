1b81c82f5dbc6e3fb34385218ab1e6b61a223a73
Red-Asuka <liyd0623@gmail.com>
2024-11-20 09:36:14 +0800

    docs(i18n): synchronized translations

--- packages/ui/src/i18n/log.ts
+++ packages/ui/src/i18n/log.ts
@@ -6,4 +6,18 @@ export default {
     tr: 'Günlük',
     hu: 'Napló',
   },
+  logLevel: {
+    zh: '日志等级',
+    en: 'Log Level',
+    ja: 'ログレベル',
+    tr: 'Günlük Seviyesi',
+    hu: 'Napló Szint',
+  },
+  logLevelDesc: {
+    zh: '等级：DEBUG > INFO > WARN > ERROR',
+    en: 'Levels: DEBUG > INFO > WARN > ERROR',
+    ja: 'レベル：DEBUG > INFO > WARN > ERROR',
+    tr: 'Seviyeler: DEBUG > INFO > WARN > ERROR',
+    hu: 'Szintek: DEBUG > INFO > WARN > ERROR',
+  },
 }
