fcd805773f900dccc0cdbaeedd91e38a74a8691f
Red-Asuka <liyd0623@gmail.com>
2024-12-03 16:13:01 +0800

    feat(devtools): open DevTools automatically in development mode

--- apps/desktop/src/main/index.ts
+++ apps/desktop/src/main/index.ts
@@ -24,6 +24,9 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
+    if (is.dev) {
+      mainWindow.webContents.openDevTools()
+    }
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
