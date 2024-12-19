c10c1fda1b8ff1f66b14f39cf1bfaff245030bb1
Red-Asuka <liyd0623@gmail.com>
2024-12-19 11:13:10 +0800

    feat(web): add installation CLI function to jump to download page

--- apps/desktop/src/renderer/src/pages/settings.vue
+++ apps/desktop/src/renderer/src/pages/settings.vue
@@ -2,8 +2,16 @@
 import useSettingsService from '@database/services/SettingsService'
 
 const { settings } = useSettingsService()
+
+function installCli() {
+  // TODO: implement this function
+  console.log('installCli')
+}
 </script>
 
 <template>
-  <SettingsView v-model="settings" />
+  <SettingsView
+    v-model="settings"
+    @install-cli="installCli"
+  />
 </template>
--- apps/web/src/pages/settings.vue
+++ apps/web/src/pages/settings.vue
@@ -2,8 +2,18 @@
 import useSettingsService from '@/database/services/SettingsService'
 
 const { settings } = useSettingsService()
+
+const { MQTTXSite } = useLinks()
+
+function installCli() {
+  const link = `${MQTTXSite.value}/cli#download`
+  window.open(link, '_blank')
+}
 </script>
 
 <template>
-  <SettingsView v-model="settings" />
+  <SettingsView
+    v-model="settings"
+    @install-cli="installCli"
+  />
 </template>
