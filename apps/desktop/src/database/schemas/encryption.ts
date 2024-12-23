6e9373a044c5640e93d6c341683db36c2f143fa1
Red-Asuka <liyd0623@gmail.com>
2024-12-23 10:40:49 +0800

    fix(desktop): fix database migration error

--- apps/desktop/src/database/schemas/encryption.ts
+++ apps/desktop/src/database/schemas/encryption.ts
@@ -1,7 +1,10 @@
 import CryptoJS from 'crypto-js'
 import { customType } from 'drizzle-orm/sqlite-core'
 
-const secretKey = import.meta.env.VITE_DB_SECRET_KEY ?? 'dah9MVR-nxp.qcw_maj'
+// FIXME: During database migration, drizzle-kit will prompt "import.meta" is not available with the "cjs" output format and will be empty
+// Temporary solution: Use process.env.VITE_DB_SECRET_KEY. Although there will still be a warning, the secretKey will be correctly assigned
+// Issue: https://github.com/drizzle-team/drizzle-orm/issues/3304
+const secretKey = import.meta.env?.VITE_DB_SECRET_KEY ?? process.env?.VITE_DB_SECRET_KEY ?? 'dah9MVR-nxp.qcw_maj'
 
 const encryptedText = customType<{ data: string, driverData: string, notNull: true, default: true }>({
   dataType() {
