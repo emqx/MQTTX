9e813a1eec2aacddbae2d11c9c78a169d3435acf
Red-Asuka <liyd0623@gmail.com>
2024-12-24 15:29:26 +0800

    feat(web): implement log4js integration and create useLog4 composable

--- apps/web/src/pages/log.vue
+++ apps/web/src/pages/log.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+const { logger, logMemory } = useLog4()
 
+logger.trace('trace')
+logger.debug('debug')
+logger.info('info')
+logger.warn('warn')
+logger.error('error')
+logger.fatal('fatal')
+logger.mark('mark')
 </script>
 
 <template>
-  <div>
-    123
-  </div>
+  <pre v-html="logMemory" />
 </template>
