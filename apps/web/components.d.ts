412ad602815d5e05bd33dcf26f81f3beb18cda5d
Red-Asuka <liyd0623@gmail.com>
2025-01-09 14:59:13 +0800

    feat(script): add schema editing feature

--- apps/web/components.d.ts
+++ apps/web/components.d.ts
@@ -61,6 +61,7 @@ declare module 'vue' {
     ScriptFunctionTest: typeof import('./../../packages/ui/src/components/script/function/Test.vue')['default']
     ScriptFunctionView: typeof import('./../../packages/ui/src/components/script/function/View.vue')['default']
     ScriptSaveDialog: typeof import('./../../packages/ui/src/components/script/SaveDialog.vue')['default']
+    ScriptSchemaEditor: typeof import('./../../packages/ui/src/components/script/schema/Editor.vue')['default']
     ScriptSchemaView: typeof import('./../../packages/ui/src/components/script/schema/View.vue')['default']
     ScriptView: typeof import('./../../packages/ui/src/components/script/View.vue')['default']
     SettingsView: typeof import('./../../packages/ui/src/components/SettingsView.vue')['default']
