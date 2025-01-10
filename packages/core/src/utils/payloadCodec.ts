472ead0e5dac763c43fcdebc976c87f4ae832876
Red-Asuka <liyd0623@gmail.com>
2025-01-10 17:03:44 +0800

    feat(avro): add protobuf codec

--- packages/core/src/utils/payloadCodec.ts
+++ packages/core/src/utils/payloadCodec.ts
@@ -1,53 +1,66 @@
-import type { ScriptSchema } from 'mqttx'
 // eslint-disable-next-line unicorn/prefer-node-protocol
 import { Buffer } from 'buffer'
+import avro from 'avsc'
 import protobuf from 'protobufjs'
 
-function getProtobufMessage(protoSchema: string, messageName: string) {
-  const root = protobuf.parse(protoSchema).root
+function getProtobufMessage(schema: string, messageName: string) {
+  const root = protobuf.parse(schema).root
   return root.lookupType(messageName)
 }
 
-export const PayloadCodec: Record<ScriptSchema['codec'], Record<'encode' | 'decode', (args: any) => Buffer>> = {
-  protobuf: {
-    encode({
-      payload,
-      protoSchema,
-      messageName,
-    }: {
-      payload: Buffer
-      protoSchema: string
-      messageName: string
-    }): Buffer {
-      const Message = getProtobufMessage(protoSchema, messageName)
-      // Avoid using json-bigint for parsing as it will lose the prototype, causing verification to fail
-      // Issue: https://github.com/sidorares/json-bigint/issues/38
-      const payloadObj = JSON.parse(payload.toString())
-      const err = Message.verify(payloadObj)
-      if (err) throw err
-      const data = Message.create(payloadObj)
-      const serializedMessage = Message.encode(data).finish()
-      return Buffer.from(serializedMessage)
-    },
+function encodeProtobuf(args: {
+  payload: Buffer
+  schema: string
+  messageName: string
+}): Buffer {
+  const Message = getProtobufMessage(args.schema, args.messageName)
+  // Avoid using json-bigint for parsing as it will lose the prototype, causing verification to fail
+  // Issue: https://github.com/sidorares/json-bigint/issues/38
+  const payloadObj = JSON.parse(args.payload.toString())
+  const err = Message.verify(payloadObj)
+  if (err) throw err
+  const data = Message.create(payloadObj)
+  const serializedMessage = Message.encode(data).finish()
+  return Buffer.from(serializedMessage)
+}
 
-    decode({
-      payload,
-      protoSchema,
-      messageName,
-    }: {
-      payload: Buffer
-      protoSchema: string
-      messageName: string
-    }): Buffer {
-      const Message = getProtobufMessage(protoSchema, messageName)
-      const MessageData = Message.decode(payload)
-      const err = Message.verify(MessageData)
-      if (err) throw err
-      return Buffer.from(JSON.stringify(MessageData))
-    },
+function decodeProtobuf(args: {
+  payload: Buffer
+  schema: string
+  messageName: string
+}): Buffer {
+  const Message = getProtobufMessage(args.schema, args.messageName)
+  const MessageData = Message.decode(args.payload)
+  const err = Message.verify(MessageData)
+  if (err) throw err
+  return Buffer.from(JSON.stringify(MessageData))
+}
+
+function encodeAvro(args: {
+  payload: Buffer
+  schema: string
+}): Buffer {
+  const AvroType = avro.Type.forSchema(JSON.parse(args.schema))
+  const payloadObj = JSON.parse(args.payload.toString())
+  return AvroType.toBuffer(payloadObj)
+}
+
+function decodeAvro(args: {
+  payload: Buffer
+  schema: string
+}): Buffer {
+  const AvroType = avro.Type.forSchema(JSON.parse(args.schema))
+  const decoded = AvroType.fromBuffer(args.payload)
+  return Buffer.from(JSON.stringify(decoded))
+}
+
+export const payloadCodec = {
+  protobuf: {
+    encode: encodeProtobuf,
+    decode: decodeProtobuf,
   },
   avro: {
-    encode: () => Buffer.from(''),
-    decode: () => Buffer.from(''),
+    encode: encodeAvro,
+    decode: decodeAvro,
   },
 }
