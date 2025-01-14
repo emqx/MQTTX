bc5c3a6aa211a69fdf7e06d8b736ce86c254b968
Red-Asuka <liyd0623@gmail.com>
2025-01-14 11:34:32 +0800

    feat(ui): add loading animation

--- packages/ui/auto-imports.d.ts
+++ packages/ui/auto-imports.d.ts
@@ -99,6 +99,8 @@ declare module 'vue' {
   interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
+    readonly ElMessage: UnwrapRef<typeof import('element-plus/es')['ElMessage']>
+    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly acceptHMRUpdate: UnwrapRef<typeof import('pinia')['acceptHMRUpdate']>
     readonly computed: UnwrapRef<typeof import('vue')['computed']>
     readonly createApp: UnwrapRef<typeof import('vue')['createApp']>
