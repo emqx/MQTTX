f5f1cade7b1f942e2d099b50f560d5841b56c02c
Red-Asuka <liyd0623@gmail.com>
2025-01-15 14:05:52 +0800

    feat(web): add environment variables for online and docker modes

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -4,6 +4,8 @@
 interface ImportMetaEnv {
   readonly VITE_WEB_DB_SECRET_KEY: string
   readonly __APP_VERSION__: string
+  readonly VITE_APP_DEFAULT_HOST: string
+  readonly VITE_APP_IS_ONLINE_ENV?: 'true' | 'false'
 }
 
 interface ImportMeta {
