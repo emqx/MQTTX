49d31b6ec41e09a47d98ff8dc1de0154a533316d
Red-Asuka <liyd0623@gmail.com>
2025-02-11 13:58:45 +0800

    improve(desktop): merge the data of settings in the store

--- apps/desktop/src/main/config/contextMenu.ts
+++ apps/desktop/src/main/config/contextMenu.ts
@@ -1,5 +1,5 @@
 import type { Options } from 'electron-context-menu'
-import type { Lang } from 'mqttx'
+import type { Lang, Settings } from 'mqttx'
 import Store from 'electron-store'
 import contextMenuLabels from './contextMenuLabels'
 
@@ -18,8 +18,9 @@ export const contextMenuConfig: Options = {
   showCopyImage: false,
   labels: getContextMenuLabels('en'),
   shouldShowMenu: (_event, parameters) => {
+    const settings = store.get('settings') as Settings | undefined
     // @ts-expect-error To support i18n, we need to set labels dynamically
-    contextMenuConfig.labels = getContextMenuLabels(store.get('currentLang') || 'en')
+    contextMenuConfig.labels = getContextMenuLabels(settings?.currentLang || 'en')
 
     // Doesn't show the menu if the link is the internal link
     const { linkURL, pageURL } = parameters
