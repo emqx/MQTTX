ac3752034a50faf6b8d044158a7cb922e7ab8980
YuShifan <894402575bt@gmail.com>
2025-03-07 16:52:20 +0800

    refactor(copilot): extract header and messages into separate components

---
+++ src/components/ai/CopilotHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="copilot-header clearfix">
+    <span>MQTTX Copilot <el-tag size="mini" type="info">Beta</el-tag></span>
+    <div>
+      <el-button type="text" @click="clearAllMessages"><i class="el-icon-delete"></i></el-button>
+      <el-button type="text" @click="toggleWindow"><i class="el-icon-close"></i></el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CopilotHeader extends Vue {
+  private clearAllMessages() {
+    this.$emit('clear-all-messages')
+  }
+
+  private toggleWindow() {
+    this.$emit('toggle-window')
+  }
+}
+</script>
+
+<style lang="scss">
+.copilot-header {
+  &.clearfix {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .el-button--text {
+      padding: 0;
+      i {
+        font-size: 16px;
+        color: var(--color-text-title);
+        font-weight: 400;
+      }
+    }
+  }
+
+  .el-tag {
+    margin-left: 8px;
+  }
+}
+</style>
