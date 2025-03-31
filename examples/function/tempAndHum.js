509b8813f1d3f27b07a6c25155ca3bae1b27ea61
Red-Asuka <liyd0623@gmail.com>
2025-03-31 17:41:54 +0800

    docs(examples): update custom function examples

--- examples/function/tempAndHum.js
+++ examples/function/tempAndHum.js
@@ -3,12 +3,15 @@ function random(min, max) {
 }
 
 /**
- * @description: Simulated dynamic switch command
- * @param {string} message - Message payload
+ * @description: Simulated dynamic temperature and humidity data
+ * @param {object} params - The parameters object
+ * @param {string} params.payload - Message payload
+ * @param {string} params.messageType - Message type, value is 'received' or 'publish'
  * @return {string} - Return a simulated temperature and humidity data - { "temperature": 23, "humidity": 40 }
  */
-function handlePayload(message) {
-  let _message = JSON.parse(message || '{}')
+function handlePayload(params) {
+  const { payload } = params
+  let _message = JSON.parse(payload || '{}')
   _message.temperature = random(10, 30)
   _message.humidity = random(20, 40)
   return JSON.stringify(_message, null, 2)
