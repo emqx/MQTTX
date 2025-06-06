d4205123f7a29b909ab42f4f1e5baf65921a6d3f
YuShifan <894402575bt@gmail.com>
2025-06-06 15:36:36 +0800

    feat(cli): update DEFAULT_MQTT_PUB_MESSAGE to 'Hello From MQTTX CLI'

--- cli/src/utils/constants.ts
+++ cli/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Maximum size of a single message in bytes
 export const MQTT_SINGLE_MESSAGE_BYTE_LIMIT = 256 * 1024 * 1024
 
-export const DEFAULT_MQTT_PUB_MESSAGE = 'Hello, World!'
+export const DEFAULT_MQTT_PUB_MESSAGE = 'Hello From MQTTX CLI'
