f128fafae72ac124b7b81d8403f7713f7553f3c8
ysfscream <894402575bt@gmail.com>
2026-01-21 14:41:53 +0800

    feat(ui): improve connection list and message item styling
    - Remove separator lines from message properties for consistent appearance
    - Add bottom border to connection list header
    - Add icons to new connection/group dropdown menu items
    - Adjust shadow and color variables in themes

--- src/views/connections/ConnectionsList.vue
+++ src/views/connections/ConnectionsList.vue
@@ -14,10 +14,10 @@
           </a>
           <el-dropdown-menu class="connection-oper-item" slot="dropdown">
             <el-dropdown-item command="newConnection">
-              {{ $t('connections.newConnections') }}
+              <i class="iconfont icon-connections"></i>{{ $t('connections.newConnections') }}
             </el-dropdown-item>
             <el-dropdown-item command="newGroup">
-              {{ $t('connections.newCollection') }}
+              <i class="el-icon-folder"></i>{{ $t('connections.newCollection') }}
             </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
@@ -782,6 +782,7 @@ export default class ConnectionsList extends Vue {
     @include flex-space-between;
     min-height: 10px;
     height: 59px;
+    border-bottom: 1px solid var(--color-border-default);
     -webkit-app-region: drag;
     a {
       -webkit-app-region: no-drag;
@@ -931,4 +932,5 @@ export default class ConnectionsList extends Vue {
     }
   }
 }
+@include el-dropdown-menu-common;
 </style>
